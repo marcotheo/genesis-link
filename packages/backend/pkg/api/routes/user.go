223da8b47f3ac2d3c4dd9665f31bf2f197ca265c
marcotheo <marcobutalid989@gmail.com>
2024-06-08 13:55:14 +0800

    added create and read user apis

--- packages/backend/pkg/api/routes/user.go
+++ packages/backend/pkg/api/routes/user.go
@@ -2,35 +2,78 @@ package userapi
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/marcotheo/genesis-fleet/packages/backend/pkg/db"
 	"github.com/marcotheo/justarouter"
 )
+
+type Response struct {
+    Status  string `json:"status"`
+    Message string `json:"message"`
+    Data    any    `json:"data,omitempty"`
+}
 
 func Routes(queries *db.Queries) func(subRouter justarouter.SubRouter) {
 	fmt.Println("running sub routes")
 
 	userRoutes :=  func(subRouter justarouter.SubRouter) {
-		subRouter.GET("/health", func(w http.ResponseWriter, r *http.Request,) {
-			fmt.Println("CREATING USER")
-
-			user, err := queries.CreateUser(context.Background(), db.CreateUserParams{
-				Userid: "434343434",
-				Firstname: "Diane",
-				Lastname: "Butalid",
-				Email: "marcobutalid@gmail.com",
-			})
-
-			fmt.Println("CREATING USER", user)
-
+		subRouter.POST("/create", func(w http.ResponseWriter, r *http.Request,) {
+			fmt.Println("USER :: CREATE")
+			
+			body, err := io.ReadAll(r.Body)
 			if err != nil {
-				fmt.Println("err %s", err)
+				http.Error(w, "Unable to read body", http.StatusBadRequest)
+				return
+			}
+			defer r.Body.Close()
+
+			var userData db.CreateUserParams
+
+			err = json.Unmarshal(body, &userData)
+			if err != nil {
+				http.Error(w, "Invalid JSON", http.StatusBadRequest)
+				return
 			}
 
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprint(w, "App is Healthy Marco")
+			userData.Userid = uuid.New().String()
+
+			user, err := queries.CreateUser(context.Background(), userData)
+			if err != nil {
+				fmt.Printf("err %s \n", err)
+			}
+
+			response, err := json.Marshal(user)
+			if  err != nil {
+				http.Error(w, "Error creating response", http.StatusInternalServerError)
+            	return
+			}
+
+			fmt.Println("USER :: CREATE SUCCESSFUL")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(response)
+		})
+
+		subRouter.GET("/{userId}", func(w http.ResponseWriter, r *http.Request,) {
+			fmt.Println("USER :: GET DETAILS")
+
+			userId := r.PathValue("userId")
+
+			user, err := queries.GetUser(context.Background(), userId)
+
+			if err != nil {
+				fmt.Printf("err %s \n", err)
+			}
+
+			fmt.Println("USER :: GET DETAILS SUCCESSFUL")
+
+			w.Header().Set("Content-Type", "application/json")
+        	json.NewEncoder(w).Encode(Response{Status: "success", Data: user})
 		})
 	}
 
