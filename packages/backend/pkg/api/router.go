b351b9018870c7b40a410d9f7329ec7be4aeba18
marcotheo <marcobutalid989@gmail.com>
2024-06-10 21:08:17 +0800

    initial router for aws deployment

--- packages/backend/pkg/api/router.go
+++ packages/backend/pkg/api/router.go
@@ -1,18 +1,31 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/joho/godotenv"
-	handler "github.com/marcotheo/genesis-fleet/packages/backend/pkg/api/handlers"
-	routes "github.com/marcotheo/genesis-fleet/packages/backend/pkg/api/routes"
 	clog "github.com/marcotheo/genesis-fleet/packages/backend/pkg/logger"
-	"github.com/marcotheo/genesis-fleet/packages/backend/pkg/services"
 	"github.com/marcotheo/justarouter"
 	"go.uber.org/dig"
 )
+
+type HealthCheckResponse struct {
+	Message string `json:"message"`
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HELLO WORLDS")
+	response := HealthCheckResponse{Message: "App is Healthysss"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
 
 func Handler() {
 	if err := godotenv.Load(); err != nil {
@@ -24,37 +37,42 @@ func Handler() {
 	container := dig.New()
 
 	// initialize services and handlers
-	container.Provide(services.InitDataService)
-	container.Provide(services.InitCognitoService)
-	container.Provide(handler.InitUserHandler)
+	// container.Provide(services.InitDataService)
+	// container.Provide(services.InitCognitoService)
+	// container.Provide(handler.InitUserHandler)
 
 	err := container.Invoke(func(
-		dataService *services.DataService,
-		userHandler *handler.UserHandler,
+	// dataService *services.DataService,
+	// userHandler *handler.UserHandler,
 	) {
-		defer dataService.Conn.Close()
+		// defer dataService.Conn.Close()
 
 		router := justarouter.CreateRouter()
 
-		router.AddSubRoutes("/user", routes.User(userHandler))
+		// router.AddSubRoutes("/user", routes.User(userHandler))
 
-		router.GET("/health", func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprint(w, "App is Healthy")
-		})
+		router.GET("/health", healthCheckHandler)
 
-		server := http.Server{
-			Addr:    ":8080",
-			Handler: router.Mux,
+		deployment := os.Getenv("GO_DEPLOMENT")
+
+		if deployment == "server" {
+			server := http.Server{
+				Addr:    ":3000",
+				Handler: router.Mux,
+			}
+
+			clog.Logger.Info("Starting server on :3000")
+
+			err := server.ListenAndServe()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			adapter := httpadapter.New(router.Mux)
+			lambda.Start(adapter.ProxyWithContext)
 		}
 
-		clog.Logger.Info("Starting server on :8080")
-
-		err := server.ListenAndServe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
 	})
 
 	if err != nil {
