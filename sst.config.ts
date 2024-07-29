8356c715b712d3d5d64d4ee06e521f242f59d93d
marcotheo <marcobutalid989@gmail.com>
2024-07-29 15:57:09 +0800

    sst script modified

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,8 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 import { main_backend } from "./infra/main-backend";
 import { main_user_pool } from "./infra/main-userpool";
-
+import { images_cdn } from "./infra/images-cdn";
+import { cloudflare_pages } from "./infra/cloudflare-pages";
 export default $config({
   app(input) {
     return {
@@ -14,23 +14,51 @@ export default $config({
           profile: process.env.PROFILE,
           region: process.env.AWS_REGION as any,
         },
+        cloudflare: true,
       },
     };
   },
   async run() {
     const mainUserPool = main_user_pool();
+
     const mainBackendResult = await main_backend({
       poolId: mainUserPool.pool.id,
       poolClientId: mainUserPool.client.id,
-      poolClientSecret: mainUserPool.clientSecret
+      poolClientSecret: mainUserPool.clientSecret,
     });
 
-    return {
+    let output = {
       roleName: mainBackendResult.role.name,
       apiUrl: mainBackendResult.api.apiEndpoint,
       poolId: mainUserPool.pool.id,
       poolClient: mainUserPool.client.id,
       poolClientSecret: mainUserPool.clientSecret,
     };
+
+    if (!process.env.APP_NAME) {
+      console.error("must define APP_NAME env variable");
+      return;
+    }
+
+    const cdnInfra = images_cdn();
+
+    output = {
+      ...output,
+      ...cdnInfra,
+    };
+
+    if (
+      process.env.NODE_ENV &&
+      ["production", "preview"].includes(process.env.NODE_ENV)
+    ) {
+      const cloudflareResults = cloudflare_pages();
+
+      output = {
+        ...output,
+        ...cloudflareResults,
+      };
+    }
+
+    return output;
   },
 });
