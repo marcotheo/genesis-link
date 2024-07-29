b3c56a8ac701d2414dd022c4d6348fdbc98e54ab
marcotheo <marcobutalid989@gmail.com>
2024-07-29 23:20:54 +0800

    modified migrations

--- packages/backend/pkg/sqlc/migrations/20240729113528_alter_user_table.sql
+++ packages/backend/pkg/sqlc/migrations/20240729113528_alter_user_table.sql
@@ -1,5 +1,7 @@
 -- +goose Up
-ALTER TABLE users ADD COLUMN password VARCHAR(255) NOT NULL;
+ALTER TABLE users ADD COLUMN password VARCHAR(255);
+ALTER TABLE users ADD COLUMN salt VARCHAR(255);
 
 -- +goose Down
 ALTER TABLE users DROP COLUMN password;
+ALTER TABLE users DROP COLUMN salt;
