0cf90a92b94d961517b0cc38e7228e3149c80ac3
marcotheo <marcobutalid989@gmail.com>
2024-07-30 00:01:09 +0800

    modified migrations

--- packages/backend/pkg/sqlc/migrations/20240606130919_create_user_table.sql
+++ packages/backend/pkg/sqlc/migrations/20240606130919_create_user_table.sql
@@ -2,9 +2,12 @@
 -- +goose StatementBegin
 CREATE TABLE users (
     userId UUID NOT NULL PRIMARY KEY,
-    firstName varchar(255) NOT NULL,
-    lastName varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT,
+    salt TEXT,
+    google_id TEXT UNIQUE,
     created_at DEFAULT CURRENT_TIMESTAMP,
     updated_at DEFAULT CURRENT_TIMESTAMP
 );
--- packages/backend/pkg/sqlc/migrations/20240729113528_alter_user_table.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE users ADD COLUMN password VARCHAR(255);
-ALTER TABLE users ADD COLUMN salt VARCHAR(255);
-
--- +goose Down
-ALTER TABLE users DROP COLUMN password;
-ALTER TABLE users DROP COLUMN salt;
