3496b9a440557cec325faecce676f96cdfef3e84
marcotheo <marcobutalid989@gmail.com>
2024-07-31 00:55:16 +0800

    sst types

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,6 +3,15 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
+    GenesisLinkMainUserClient: {
+      id: string
+      secret: string
+      type: "sst.aws.CognitoUserPoolClient"
+    }
+    GenesisLinkMainUserPool: {
+      id: string
+      type: "sst.aws.CognitoUserPool"
+    }
   }
 }
 export {}
