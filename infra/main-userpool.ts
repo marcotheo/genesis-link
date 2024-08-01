3b57c0be62c94bf9399517c90717a6a4d54b0b5e
marcotheo <marcobutalid989@gmail.com>
2024-08-01 13:42:24 +0800

    added custom message lambda

--- infra/main-userpool.ts
+++ infra/main-userpool.ts
@@ -1,31 +1,87 @@
+import path = require("path");
+
 export const main_user_pool = () => {
-  const pool = new sst.aws.CognitoUserPool(
-    `${process.env.PROJ_NAME}MainUserPool`
+  // Create an IAM Role for the Lambda function
+  const lambdaRole = new aws.iam.Role("SignUpCognitoLambdaRole", {
+    assumeRolePolicy: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "sts:AssumeRole",
+          Principal: {
+            Service: "lambda.amazonaws.com",
+          },
+          Effect: "Allow",
+        },
+      ],
+    },
+  });
+
+  // Attach a policy to the IAM Role to allow sending emails via an external service
+  new aws.iam.RolePolicy("SignUpCognitoLambdaRolePolicy", {
+    role: lambdaRole.id,
+    policy: JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          Action: [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+          ],
+          Resource: "*",
+        },
+      ],
+    }),
+  });
+
+  const filePath = `${process.cwd()}/packages/events/signup/deployment.zip`;
+
+  // Create a Lambda function for custom email sending
+  const signUpLambdaFunction = new aws.lambda.Function(
+    "CognitoCustomMessageLambda",
+    {
+      runtime: aws.lambda.Runtime.CustomAL2023,
+      role: lambdaRole.arn,
+      handler: "bootstrap", // Custom runtimes can have handler set to an empty string ""
+      code: new $util.asset.FileArchive(filePath),
+    }
   );
 
-  const client = pool.addClient(`${process.env.PROJ_NAME}MainUserClient`, {
-    transform: {
-      client: {
-        explicitAuthFlows: [
-          "ALLOW_USER_PASSWORD_AUTH",
-          "ALLOW_REFRESH_TOKEN_AUTH",
-        ],
-        tokenValidityUnits: {
-          accessToken: "minutes",
-          idToken: "minutes",
-          refreshToken: "hours",
-        },
-        accessTokenValidity: 10,
-        idTokenValidity: 10,
-        refreshTokenValidity: 3,
-        generateSecret: true,
-      },
+  const pool = new aws.cognito.UserPool("UserPool", {
+    usernameAttributes: ["email"],
+    lambdaConfig: {
+      customMessage: signUpLambdaFunction.arn,
     },
+  });
+
+  const client = new aws.cognito.UserPoolClient("UserPoolClient", {
+    userPoolId: pool.id,
+    explicitAuthFlows: ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"],
+    tokenValidityUnits: {
+      accessToken: "minutes",
+      idToken: "minutes",
+      refreshToken: "hours",
+    },
+    accessTokenValidity: 10,
+    idTokenValidity: 10,
+    refreshTokenValidity: 3,
+    generateSecret: true,
+  });
+
+  // Grant permission for Cognito to invoke the Lambda function
+  new aws.lambda.Permission("CognitoLambdaInvokePermission", {
+    action: "lambda:InvokeFunction",
+    function: signUpLambdaFunction,
+    principal: "cognito-idp.amazonaws.com",
+    sourceArn: pool.arn,
   });
 
   return {
     poolId: pool.id,
     poolClientId: client.id,
-    poolClientSecret: client.nodes.client.clientSecret,
+    poolClientSecret: client.clientSecret,
+    signUpLambdaFunction: signUpLambdaFunction.name,
   };
 };
--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,15 +3,6 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    GenesisLinkMainUserClient: {
-      id: string
-      secret: string
-      type: "sst.aws.CognitoUserPoolClient"
-    }
-    GenesisLinkMainUserPool: {
-      id: string
-      type: "sst.aws.CognitoUserPool"
-    }
   }
 }
 export {}
