5ab89cde79600929ea8ffb9acc2918f7237a6999
marcotheo <marcobutalid989@gmail.com>
2024-08-03 16:46:00 +0800

    added alert component with success & error variant

---
+++ packages/frontend/src/components/alert/alert.tsx
@@ -0,0 +1,44 @@
+import { CheckIcon, ErrorIcon } from "../icons/icons";
+import { component$ } from "@builder.io/qwik";
+import { cn } from "~/common/utils";
+
+type Variant = "success" | "error";
+
+export const Icon = component$<{ variant: Variant }>(({ variant }) => {
+  if (variant === "success") return <CheckIcon class="size-10 text-success" />;
+
+  return <ErrorIcon class="size-10 text-destructive" />;
+});
+
+interface Props {
+  variant: Variant;
+  title: string;
+  message: string;
+  open: boolean;
+}
+
+export default component$<Props>((props) => {
+  const variantBorder = {
+    success: "border-success",
+    error: "border-destructive",
+  };
+
+  return (
+    <div
+      class={cn(
+        "flex items-center gap-3 p-3",
+        "rounded-r-sm rounded-br-sm",
+        "border-l-4 bg-surface shadow-md",
+        variantBorder[props.variant],
+        !!props.open ? "animate-fade-in-slide" : "animate-fade-out-slide",
+      )}
+    >
+      <Icon variant={props.variant} />
+
+      <div>
+        <p class="text-lg">{props.title}</p>
+        <p class="text-sm">{props.message}</p>
+      </div>
+    </div>
+  );
+});
