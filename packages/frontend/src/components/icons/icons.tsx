c5f923c3f63cc1b0ecb66d5c853e41d7151d2a1b
marcotheo <marcobutalid989@gmail.com>
2024-08-03 16:46:32 +0800

    added error and check icon

--- packages/frontend/src/components/icons/icons.tsx
+++ packages/frontend/src/components/icons/icons.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import { cn } from "~/common/utils";
 
 export const HamburgerIcon = component$(() => {
   return (
@@ -52,6 +53,40 @@ export const SignUpIcon = component$(() => {
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+      />
+    </svg>
+  );
+});
+
+export const CheckIcon = component$<{ class?: string }>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={cn("size-6", props.class)}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+});
+
+export const ErrorIcon = component$<{ class?: string }>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={cn("size-6", props.class)}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+        clip-rule="evenodd"
       />
     </svg>
   );
