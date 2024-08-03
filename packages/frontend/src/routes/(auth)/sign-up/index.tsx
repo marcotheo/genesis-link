a583e603eea2c7df87b40ce5ffda755daafcaa31
marcotheo <marcobutalid989@gmail.com>
2024-08-03 18:11:46 +0800

    signup page

--- packages/frontend/src/routes/(auth)/sign-up/index.tsx
+++ packages/frontend/src/routes/(auth)/sign-up/index.tsx
@@ -1,42 +1,199 @@
+import {
+  InitialValues,
+  SubmitHandler,
+  useForm,
+  valiForm$,
+} from "@modular-forms/qwik";
+import { DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
+import * as v from "valibot";
+
+import LoadingOverlay from "~/components/loading-overlay/loading-overlay";
+import { useMutate } from "~/hooks/use-mutate/useMutate";
 import Checkbox from "~/components/checkbox/checkbox";
-import { DocumentHead } from "@builder.io/qwik-city";
+import { cn, RegexValidations } from "~/common/utils";
 import Heading from "~/components/heading/heading";
+import { $, component$ } from "@builder.io/qwik";
 import Button from "~/components/button/button";
-import { component$ } from "@builder.io/qwik";
 import Input from "~/components/input/input";
-import { cn } from "~/common/utils";
+import Alert from "~/components/alert/alert";
+
+interface Response {
+  status: string;
+  message: string;
+  data: {
+    email: string;
+    password: string;
+  };
+}
+
+const SignUpSchema = v.pipe(
+  v.object({
+    email: v.pipe(
+      v.string(),
+      v.nonEmpty("Please enter your email."),
+      v.email("The email address is badly formatted."),
+    ),
+    password: v.pipe(
+      v.string(),
+      v.nonEmpty("Please enter your password."),
+      v.minLength(8, "Your password must have 8 characters or more."),
+      v.maxLength(64, "Your password must not have more than 64 characters"),
+      v.regex(
+        RegexValidations.hasSpecialChar,
+        "Your password must have special character",
+      ),
+      v.regex(
+        RegexValidations.hasLowerCase,
+        "Your password must have lower case",
+      ),
+      v.regex(
+        RegexValidations.hasUpperCase,
+        "Your password must have upper case",
+      ),
+      v.regex(RegexValidations.hasNumber, "Your password must have a number"),
+    ),
+    confirmPassword: v.pipe(
+      v.string(),
+      v.nonEmpty("Please confirm your password."),
+    ),
+    consent: v.boolean(),
+  }),
+  v.forward(
+    v.check(
+      ({ password, confirmPassword }) => password === confirmPassword,
+      "Password do not match",
+    ),
+    ["confirmPassword"],
+  ),
+  v.forward(
+    v.check(
+      ({ consent }) => !!consent,
+      "Can't proceed withtout accepting terms",
+    ),
+    ["consent"],
+  ),
+);
+
+type SignUpForm = v.InferInput<typeof SignUpSchema>;
+
+export const useFormLoader = routeLoader$<InitialValues<SignUpForm>>(() => ({
+  email: "",
+  password: "",
+  confirmPassword: "",
+  consent: false,
+}));
 
 export default component$(() => {
+  const { mutate, state } = useMutate<Response>("/user/create");
+
+  const [_, { Form, Field }] = useForm<SignUpForm>({
+    loader: useFormLoader(),
+    validate: valiForm$(SignUpSchema),
+  });
+
+  const handleSubmit = $<SubmitHandler<SignUpForm>>(async (values) => {
+    try {
+      const response = await mutate({
+        email: values.email,
+        password: values.password,
+      });
+      if (response.data)
+        console.log("Form submitted successfully:", response.data.data);
+      if (response.error) console.log("Error form", response.error);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  });
+
   return (
     <div class="flex h-full w-full justify-center">
-      <div class={cn("flex flex-col gap-5", "w-[500px] md:mt-[70px]")}>
+      <LoadingOverlay open={state.loading}>
+        Processing Registration
+      </LoadingOverlay>
+
+      <div class={cn("w-[500px] md:mt-[70px]")}>
         <Heading>Sign Up</Heading>
-        <div class=" flex flex-col gap-5">
-          <Input label="Email" variant="filled" />
-          <Input type="password" label="Password" variant="filled" />
-          <Input type="password" label="Confirm Password" variant="filled" />
 
-          <Checkbox name="consent">
-            <p class="max-sm:text-sm">
-              By registering, you agree to the processing of your personal data
-              by Genesis Oppurtunities as described in the{" "}
-              <span class="text-primary">Privacy Policy</span>.{" "}
-            </p>
-          </Checkbox>
+        <br />
 
-          <Button>Sign up</Button>
-        </div>
+        <Alert
+          open={!!state.success}
+          variant="success"
+          title="Success"
+          message="email verification sent"
+        />
+        <Alert
+          open={!!state.error}
+          variant="error"
+          title="Error"
+          message={state.error ?? ""}
+        />
+
+        <Form class="flex flex-col gap-5" onSubmit$={handleSubmit}>
+          <Field name="email">
+            {(field, props) => (
+              <Input
+                {...props}
+                type="email"
+                label="Email"
+                variant="filled"
+                errorMsg={field.error}
+                value={field.value}
+              />
+            )}
+          </Field>
+
+          <Field name="password">
+            {(field, props) => (
+              <Input
+                {...props}
+                type="password"
+                label="Password"
+                variant="filled"
+                errorMsg={field.error}
+                value={field.value}
+              />
+            )}
+          </Field>
+
+          <Field name="confirmPassword">
+            {(field, props) => (
+              <Input
+                {...props}
+                type="password"
+                label="Confirm Password"
+                variant="filled"
+                errorMsg={field.error}
+                value={field.value}
+              />
+            )}
+          </Field>
+
+          <Field name="consent" type="boolean">
+            {(field, props) => (
+              <Checkbox inputProps={props} errorMsg={field.error}>
+                <p class="max-sm:text-sm">
+                  By registering, you agree to the processing of your personal
+                  data by Genesis Link as described in the{" "}
+                  <span class="text-primary">Privacy Policy</span>.{" "}
+                </p>
+              </Checkbox>
+            )}
+          </Field>
+
+          <Button type="submit">Sign up</Button>
+        </Form>
       </div>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Sign up - Genesis Oppurtunities",
+  title: "Sign up - Genesis Link",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Sign Upp here at Genesis Link",
     },
   ],
 };
