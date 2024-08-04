5f1233f2913bcf5c5378acb4379ee7923b6792cf
marcotheo <marcobutalid989@gmail.com>
2024-08-04 15:55:49 +0800

    modified signup lambda

--- packages/events/signup/main.go
+++ packages/events/signup/main.go
@@ -3,24 +3,27 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Event struct {
-	// Request struct {
-	// 	UserAttributes map[string]string `json:"userAttributes"`
-	// 	CodeParameter  string            `json:"codeParameter"`
-	// } `json:"request"`
-}
-
 func handler(ctx context.Context, event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEventUserPoolsCustomMessage, error) {
+	siteURL := os.Getenv("SITE_URL")
+
 	email := event.Request.UserAttributes["email"]
 	confirmationCode := event.Request.CodeParameter
 
-	fmt.Printf("This is the email %v \n", email)
-	fmt.Printf("This is the confirmationCode %v \n", confirmationCode)
+	if siteURL == "" {
+		siteURL = "http://localhost:5173" // default value if the env variable is not set
+	}
+
+	verificationURL := fmt.Sprintf("%s/user/verify?username=%s&code=%s", siteURL, email, confirmationCode)
+
+	// Set the custom message with the verification URL
+	event.Response.EmailMessage = fmt.Sprintf("Please confirm your email address by clicking on the following link: %s", verificationURL)
+	event.Response.SMSMessage = fmt.Sprintf("Please confirm your email address by clicking on the following link: %s", verificationURL)
 
 	return event, nil
 }
