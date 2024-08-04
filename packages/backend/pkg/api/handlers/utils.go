90b248c5bea1c2e2b910152e00d458072c1e726a
marcotheo <marcobutalid989@gmail.com>
2024-08-05 00:31:53 +0800

    added encryption for auth session

--- packages/backend/pkg/api/handlers/utils.go
+++ packages/backend/pkg/api/handlers/utils.go
@@ -1,10 +1,17 @@
 package handler
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -52,4 +59,80 @@ func errorResponse(w http.ResponseWriter, statusCode int, message string) {
 func successResponse(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Response{Status: "success", Data: data})
+}
+
+func encrypt(data interface{}) (string, error) {
+	plainText, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+
+	key := getSecretKey()
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func decrypt(encrypted string, v interface{}) error {
+	cipherText, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return err
+	}
+
+	key := getSecretKey()
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(cipherText) < nonceSize {
+		return fmt.Errorf("ciphertext too short")
+	}
+
+	nonce, cipherText := cipherText[:nonceSize], cipherText[nonceSize:]
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(plainText, v)
+	return err
+}
+
+func getSecretKey() []byte {
+	secretKeyHex := os.Getenv("AUTH_SESSION_SECRET")
+	if len(secretKeyHex) != 64 {
+		log.Fatal("AUTH_SESSION_SECRET environment variable must be 64 hex characters (32 bytes)")
+	}
+	secretKey, err := hex.DecodeString(secretKeyHex)
+	if err != nil {
+		log.Fatal("Invalid hex key: ", err)
+	}
+	return secretKey
+}
+
+type AuthSession struct {
+	RefreshToken string `json:"refresh_token"`
+	Email        string `json:"email"`
+	Sub          string `json:"sub"`
 }
