1d8845025e6c39983ec68f1cb1fe78ed24c7f119
marcotheo <marcobutalid989@gmail.com>
2024-08-04 22:23:54 +0800

    small updates regarding origins allowed

--- packages/backend/pkg/api/router.go
+++ packages/backend/pkg/api/router.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/joho/godotenv"
@@ -67,9 +69,33 @@ func GetLambdaAdapter() (*httpadapter.HandlerAdapterV2, *sql.DB) {
 	return adapterLambda, dbConn
 }
 
+func getAllowedOrigins() []string {
+	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
+	if allowedOrigins == "" {
+		return []string{"*"} // Default to allow all origins
+	}
+	return strings.Split(allowedOrigins, ",")
+}
+
+func isOriginAllowed(origin string, allowedOrigins []string) bool {
+	for _, allowedOrigin := range allowedOrigins {
+		if origin == allowedOrigin || allowedOrigin == "*" {
+			return true
+		}
+	}
+	return false
+}
+
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := getAllowedOrigins()
+
+		if isOriginAllowed(origin, allowedOrigins) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "")
+		}
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
