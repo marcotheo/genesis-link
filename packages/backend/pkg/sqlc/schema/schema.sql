edf3a1ffe52f9cc1413481ead0ea77d1469fa766
marcotheo <marcobutalid989@gmail.com>
2024-08-15 00:23:32 +0800

    added post table with sqlc

--- packages/backend/pkg/sqlc/schema/schema.sql
+++ packages/backend/pkg/sqlc/schema/schema.sql
@@ -4,3 +4,20 @@ CREATE TABLE users (
     created_at DEFAULT CURRENT_TIMESTAMP,
     updated_at DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE posts (
+    postId TEXT NOT NULL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    postType TEXT CHECK(postType IN ('job', 'volunteer')) NOT NULL,
+    jobType TEXT CHECK(jobType IN ('full-time', 'part-time', 'contract', 'internship')),
+    company TEXT,
+    location TEXT,
+    salary TEXT,
+    wfh INTEGER DEFAULT 0,
+    email TEXT,
+    phone TEXT,
+    deadline DATE,
+    posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
