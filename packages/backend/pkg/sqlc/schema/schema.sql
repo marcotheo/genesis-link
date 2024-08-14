293014f8d28301d621e906e45558b2913acae745
marcotheo <marcobutalid989@gmail.com>
2024-08-15 00:24:11 +0800

    added the indexes for posts table with sqlc

--- packages/backend/pkg/sqlc/schema/schema.sql
+++ packages/backend/pkg/sqlc/schema/schema.sql
@@ -21,3 +21,9 @@ CREATE TABLE posts (
     posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX idx_postType ON posts(postType);
+CREATE INDEX idx_jobType ON posts(jobType);
+CREATE INDEX idx_wfh ON posts(wfh);
+CREATE INDEX idx_location ON posts(location);
+CREATE INDEX idx_posted_at ON posts(posted_at);
