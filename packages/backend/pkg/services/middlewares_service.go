e08dd1225f6ac4816f4ffde443f598c1e4f3cad8
marcotheo <marcobutalid989@gmail.com>
2024-08-18 12:20:21 +0800

    added middlewares_service

---
+++ packages/backend/pkg/services/middlewares_service.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+)
+
+type MiddlewareService struct {
+	cognitoService *CognitoService
+}
+
+func InitMiddlewareService(cognitoService *CognitoService) *MiddlewareService {
+	return &MiddlewareService{cognitoService}
+}
+
+// verifyToken verifies the JWT token using Cognito
+func verifyToken(client *cognitoidentityprovider.Client, token string) (*cognitoidentityprovider.GetUserInput, error) {
+	// The token is used to get the user information
+	// This is called to check if AccessToken is valid
+	_, err := client.GetUser(context.TODO(), &cognitoidentityprovider.GetUserInput{
+		AccessToken: aws.String(token),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (m *MiddlewareService) AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// Extract token from "Bearer <token>"
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		if token == authHeader {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// Verify the token
+		_, err := verifyToken(m.cognitoService.Client, token)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
