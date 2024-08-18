32dfaea23ed1483bf701d0db37efee322e6a0726
marcotheo <marcobutalid989@gmail.com>
2024-08-18 14:01:58 +0800

    added new button variant

--- packages/frontend/src/components/button/button.tsx
+++ packages/frontend/src/components/button/button.tsx
@@ -3,7 +3,7 @@ import { cn } from "~/common/utils";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: "lg" | "md" | "sm" | "default";
-  variant?: "outline" | "ghost" | "default";
+  variant?: "outline" | "ghost" | "transparent" | "default";
 }
 
 export default component$<ButtonProps>(
@@ -18,23 +18,53 @@ export default component$<ButtonProps>(
     const variants = {
       outline: "bg-transparent border border-primary hover:bg-primary-soft",
       ghost: "bg-primary-soft hover:brightness-150",
+      transparent: "bg-transparent text-text rounded-none",
       default: "bg-primary hover:bg-primary-foreground text-white",
     };
 
+    if (variant !== "transparent")
+      return (
+        <button
+          {...props}
+          class={cn(
+            "peer w-full font-primary",
+            "rounded-md",
+            "duration-150 ease-linear",
+            sizes[size],
+            variants[variant],
+            props.class,
+          )}
+        >
+          <Slot />
+        </button>
+      );
+
     return (
-      <button
-        {...props}
-        class={cn(
-          "font-primary",
-          "rounded-md",
-          "duration-150 ease-linear",
-          sizes[size],
-          variants[variant],
-          props.class,
-        )}
-      >
-        <Slot />
-      </button>
+      <div class="relative w-full">
+        <button
+          {...props}
+          class={cn(
+            "peer w-full font-primary",
+            "rounded-md",
+            "duration-150 ease-linear",
+            sizes[size],
+            variants[variant],
+            props.class,
+          )}
+        >
+          <Slot />
+        </button>
+
+        <div
+          class={cn(
+            "absolute bottom-0",
+            "w-full h-[2px]",
+            "duration-300",
+            "scale-0 bg-primary",
+            "peer-hover:scale-100",
+          )}
+        />
+      </div>
     );
   },
 );
