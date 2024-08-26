35a44d60c592fb3d30afd99c1f0b1567bd8a0d76
marcotheo <marcobutalid989@gmail.com>
2024-08-26 18:46:24 +0800

    applied csp headers for xss security

---
+++ packages/frontend/src/routes/plugin@csp.ts
@@ -0,0 +1,23 @@
+import type { RequestHandler } from "@builder.io/qwik-city";
+import { baseApiUrl, baseCDNUrl } from "~/common/constants";
+import { isDev } from "@builder.io/qwik/build";
+
+export const onRequest: RequestHandler = (event) => {
+  if (isDev) return; // Will not return CSP headers in dev mode
+  const nonce = crypto.randomUUID().replace(/-/g, ""); // Your custom nonce logic here
+  event.sharedMap.set("@nonce", nonce);
+
+  const csp = [
+    `default-src 'self'`,
+    `script-src 'self' 'nonce-${nonce}'`,
+    `connect-src 'self' ${baseApiUrl}`,
+    `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
+    `font-src 'self' https://fonts.gstatic.com`,
+    `img-src 'self' 'unsafe-inline' ${baseCDNUrl} data:`,
+    `frame-src 'self' 'nonce-${nonce}'`,
+    `object-src 'none'`,
+    `base-uri 'self'`,
+  ];
+
+  event.headers.set("Content-Security-Policy", csp.join("; "));
+};
