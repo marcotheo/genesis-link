65e4ec3c03fc2b542098ee9a699f8cc04bfea852
marcotheo <marcobutalid989@gmail.com>
2024-08-29 04:15:57 +0800

    optimized table component

--- packages/frontend/src/components/table/table.tsx
+++ packages/frontend/src/components/table/table.tsx
@@ -1,6 +1,19 @@
 import { component$, QRL, Slot, JSXOutput } from "@builder.io/qwik";
 import { cn } from "~/common/utils";
 
+export type RowDefKeyOf<ObjectType extends object> = {
+  [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
+    ? `${Key}` | `${Key}.${RowDefKeyOf<ObjectType[Key]>}`
+    : `${Key}`;
+}[keyof ObjectType & (string | number)];
+
+interface ITableProps<T extends object> {
+  headers: string[];
+  data: T[];
+  rowDef: (RowDefKeyOf<T> | QRL<(item: T) => JSXOutput>)[];
+  loading?: boolean | null;
+}
+
 const Th = component$(() => {
   return (
     <th class="p-3 text-left whitespace-nowrap">
@@ -25,91 +38,88 @@ const RowSkeleton = component$(() => (
   </td>
 ));
 
-const TableSkeleton = component$(() => {
+const TableSkeleton = component$<{ total: number }>(({ total }) => {
   return (
     <>
       {[...Array(10)].map((_, index) => (
         <tr key={index} class="border-b border-soft">
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
+          {[...Array(total)].map((_, index) => (
+            <RowSkeleton key={index} />
+          ))}
         </tr>
       ))}
     </>
   );
 });
 
-// const TableBody = component$(() => {});
+const TableHeaders = component$<{ headers: string[] }>(({ headers }) => {
+  return headers.map((value) => <Th key={value}>{value}</Th>);
+});
 
-export type RowDefKeyOf<ObjectType extends object> = {
-  [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
-    ? `${Key}` | `${Key}.${RowDefKeyOf<ObjectType[Key]>}`
-    : `${Key}`;
-}[keyof ObjectType & (string | number)];
-
-interface ITableProps<T extends object> {
-  headers: string[];
-  data: T[];
-  rowDef: (RowDefKeyOf<T> | QRL<(item: T) => JSXOutput>)[];
-  loading?: boolean | null;
-}
-
-export const Table = component$(
+const TableBody = component$(
   <T extends object>({ headers, loading, data, rowDef }: ITableProps<T>) => {
     return (
-      <div class="overflow-x-auto">
-        <table
-          class={cn(
-            "w-full min-w-[800px] rounded-lg overflow-hidden",
-            "table border-collapse",
-          )}
-        >
-          <thead>
-            <tr class="brightness-125 shadow-md">
-              {headers.map((value) => (
-                <Th key={value}>{value}</Th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {loading ? (
-              <TableSkeleton />
-            ) : (
-              data.map((item, idx) => {
-                return (
-                  <tr
-                    key={idx}
-                    class="border-b border-soft cursor-pointer hover:bg-soft duration-200 ease-out"
-                  >
-                    {rowDef.map((property, idx) => {
-                      let value: any = item;
+      <>
+        {loading ? (
+          <TableSkeleton total={headers.length} />
+        ) : (
+          data.map((item, idx) => {
+            return (
+              <tr
+                key={idx}
+                class={cn(
+                  "border-b border-soft",
+                  "cursor-pointer hover:bg-soft",
+                  "duration-200 ease-out",
+                )}
+              >
+                {rowDef.map((property, idx) => {
+                  let value: any = item;
 
-                      if (typeof property === "string") {
-                        const keys = property.split(".");
-                        for (const key of keys) {
-                          if (value && key in value) {
-                            value = value[key];
-                          } else {
-                            value = undefined;
-                            break;
-                          }
-                        }
-
-                        return <Td key={idx}>{value}</Td>;
+                  if (typeof property === "string") {
+                    const keys = property.split(".");
+                    for (const key of keys) {
+                      if (value && key in value) {
+                        value = value[key];
+                      } else {
+                        value = undefined;
+                        break;
                       }
+                    }
 
-                      return <Td key={idx}>{property(item)}</Td>;
-                    })}
-                  </tr>
-                );
-              })
-            )}
-          </tbody>
-        </table>
-      </div>
+                    return <Td key={idx}>{value}</Td>;
+                  }
+
+                  return <Td key={idx}>{property(item)}</Td>;
+                })}
+              </tr>
+            );
+          })
+        )}
+      </>
     );
   },
 );
+
+export const Table = component$(<T extends object>(props: ITableProps<T>) => {
+  return (
+    <div class="overflow-x-auto">
+      <table
+        class={cn(
+          "w-full min-w-[800px]",
+          "rounded-lg overflow-hidden",
+          "table border-collapse",
+        )}
+      >
+        <thead>
+          <tr class="brightness-125 shadow-md">
+            <TableHeaders headers={props.headers} />
+          </tr>
+        </thead>
+        <tbody>
+          <TableBody {...props} />
+        </tbody>
+      </table>
+    </div>
+  );
+});
