a3f5ae22d09e63ba15858ce0123b8800509c4197
marcotheo <marcobutalid989@gmail.com>
2024-10-29 14:42:35 +0800

    added active link in settings navigation

--- packages/frontend/src/routes/(protected)/settings/layout.tsx
+++ packages/frontend/src/routes/(protected)/settings/layout.tsx
@@ -1,13 +1,34 @@
+import { Link, useLocation } from "@builder.io/qwik-city";
 import { component$, Slot } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
 import { cn } from "~/common/utils";
 
 const Item = component$<{ link: string; title: string }>(({ link, title }) => {
+  const location = useLocation();
+  const toPathname = link ?? "";
+  const locationPathname = location.url.pathname;
+
+  const startSlashPosition =
+    toPathname !== "/" && toPathname.startsWith("/")
+      ? toPathname.length - 1
+      : toPathname.length;
+  const endSlashPosition =
+    toPathname !== "/" && toPathname.endsWith("/")
+      ? toPathname.length - 1
+      : toPathname.length;
+  const isActive =
+    locationPathname === toPathname ||
+    (locationPathname.endsWith(toPathname) &&
+      (locationPathname.charAt(endSlashPosition) === "/" ||
+        locationPathname.charAt(startSlashPosition) === "/"));
+
   return (
     <li
       class={cn(
         "dark:hover:text-white hover:text-gray-800",
-        "duration-300 dark:text-text text-gray-500",
+        "duration-300",
+        isActive
+          ? "dark:text-white text-gray-800"
+          : "dark:text-text text-gray-500",
       )}
     >
       <Link href={link}>{title}</Link>
