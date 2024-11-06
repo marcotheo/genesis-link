83f3b42056380513174748ef5cd268b853944034
marcotheo <marcobutalid989@gmail.com>
2024-11-06 12:04:26 +0800

    add common types

---
+++ packages/frontend/src/common/types.ts
@@ -0,0 +1,16 @@
+export type Address = {
+  Addressid: string;
+  Country: string;
+  Region: string;
+  Province: string;
+  City: string;
+  Barangay: string;
+  Addressdetails: string;
+};
+
+// GET API /api/v1/address
+export interface ListAddressResponse {
+  status: string;
+  message: string;
+  data: Address[];
+}
