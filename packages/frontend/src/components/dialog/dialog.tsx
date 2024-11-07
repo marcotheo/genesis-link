c8b84a9cf49d7036adb963086f3438e6db22605c
marcotheo <marcobutalid989@gmail.com>
2024-11-07 14:22:28 +0800

    small update

--- packages/frontend/src/components/dialog/dialog.tsx
+++ packages/frontend/src/components/dialog/dialog.tsx
@@ -15,72 +15,75 @@ import { cn } from "~/common/utils";
 
 interface DialogProps extends HTMLAttributes<HTMLDivElement> {
   size?: "lg" | "md" | "sm";
+  defaultOpen?: boolean;
 }
 
 export const DialogContext = createContextId<Signal<boolean | null>>(
   "dialog.open-context",
 );
 
-export default component$<DialogProps>(({ size = "sm", ...props }) => {
-  const open = useSignal<boolean | null>(null);
-  const dialogRef = useSignal<HTMLDivElement>();
+export default component$<DialogProps>(
+  ({ size = "sm", defaultOpen, ...props }) => {
+    const open = useSignal<boolean | null>(defaultOpen ?? null);
+    const dialogRef = useSignal<HTMLDivElement>();
 
-  useContextProvider(DialogContext, open);
+    useContextProvider(DialogContext, open);
 
-  const sizes = {
-    lg: "md:min-w-[40rem]",
-    md: "md:min-w-[30rem]",
-    sm: "md:min-w-[25rem]",
-  };
+    const sizes = {
+      lg: "md:min-w-[40rem]",
+      md: "md:min-w-[30rem]",
+      sm: "md:min-w-[25rem]",
+    };
 
-  useOnDocument(
-    "click",
-    $((event: any) => {
-      if (
-        open.value &&
-        dialogRef.value &&
-        !dialogRef.value.contains(event.target as Node)
-      ) {
-        open.value = false;
-      }
-    }),
-  );
+    useOnDocument(
+      "click",
+      $((event: any) => {
+        if (
+          open.value &&
+          dialogRef.value &&
+          !dialogRef.value.contains(event.target as Node)
+        ) {
+          open.value = false;
+        }
+      }),
+    );
 
-  return (
-    <div>
-      <Slot name="trigger" />
+    return (
+      <div>
+        <Slot name="trigger" />
 
-      <div
-        class={cn(
-          "fixed inset-0 w-full top-0 left-0",
-          open.value
-            ? "bg-[rgba(0,0,0,0.5)] z-[100]"
-            : "bg-transparent z-[-10]",
-        )}
-      ></div>
+        <div
+          class={cn(
+            "fixed inset-0 w-full top-0 left-0",
+            open.value
+              ? "bg-[rgba(0,0,0,0.5)] z-[100]"
+              : "bg-transparent z-[-10]",
+          )}
+        ></div>
 
-      <div
-        {...props}
-        ref={dialogRef}
-        class={cn(
-          "fixed top-0 bottom-0 left-0 right-0 m-auto",
-          "w-fit h-fit min-w-[90%] p-5",
-          "bg-surface rounded-md shadow-lg",
-          "text-text z-[100]",
-          sizes[size],
-          open.value === null
-            ? "hidden"
-            : open.value
-              ? "animate-fade-in-scale"
-              : "animate-fade-out-scale",
-          props.class,
-        )}
-      >
-        <Slot />
+        <div
+          {...props}
+          ref={dialogRef}
+          class={cn(
+            "fixed top-0 bottom-0 left-0 right-0 m-auto",
+            "w-fit h-fit min-w-[90%] p-5",
+            "bg-surface rounded-md shadow-lg",
+            "text-text z-[100]",
+            sizes[size],
+            open.value === null
+              ? "hidden"
+              : open.value
+                ? "animate-fade-in-scale"
+                : "animate-fade-out-scale",
+            props.class,
+          )}
+        >
+          <Slot />
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+);
 
 export const DialogTrigger = component$<ButtonProps>(({ ...props }) => {
   const dialogCtx = useContext(DialogContext);
