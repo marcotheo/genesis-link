1a37aeae052a775a3482851bdf11b6a7f1d2b427
marcotheo <marcobutalid989@gmail.com>
2024-11-07 14:57:19 +0800

    applied scroll to address options

--- packages/frontend/src/routes/(protected)/posts/create/Form2.tsx
+++ packages/frontend/src/routes/(protected)/posts/create/Form2.tsx
@@ -1,10 +1,12 @@
 import { $, component$, useContext, useSignal } from "@builder.io/qwik";
 
+import Dialog, { DialogTrigger } from "~/components/dialog/dialog";
 import { useQuery } from "~/hooks/use-query/useQuery";
 import { ListAddressResponse } from "~/common/types";
 import Heading from "~/components/heading/heading";
 import { FormDataCtx, FormStepCtx } from "./index";
 import Button from "~/components/button/button";
+import CreateAddress from "./CreateAddress";
 import { cn } from "~/common/utils";
 
 const AddressOptions = component$(() => {
@@ -28,38 +30,62 @@ const AddressOptions = component$(() => {
   );
 
   return (
-    <>
-      {state.loading && <div>Loading ...</div>}
-      <div class="space-y-5">
-        {state.result?.data.map((v) => (
-          <button
-            key={v.Addressid}
-            class={cn(
-              "w-full p-5",
-              "shadow-sm bg-surface rounded-md",
-              "flex justify-between items-center",
-              "cursor-pointer",
-              "ease-in duration-200",
-              selectedAddress.value === v.Addressid
-                ? "dark:brightness-150 brightness-90"
-                : "dark:hover:brightness-125 hover:brightness-95",
-            )}
-            onClick$={() => (selectedAddress.value = v.Addressid)}
-          >
-            <div class="text-left">
-              {" "}
-              <p>
-                {v.Addressdetails}, {v.Barangay},
-              </p>
-              <p>
-                {v.City}, {v.Province}, {v.Region},
-              </p>
-              <p>{v.Country},</p>
-            </div>
-          </button>
-        ))}
-      </div>
-    </>
+    <div class="space-y-5 max-h-[28rem] overflow-y-auto pr-3">
+      {state.result?.data.map((v) => (
+        <button
+          key={v.Addressid}
+          class={cn(
+            "w-full p-5",
+            "shadow-sm bg-surface rounded-md",
+            "flex justify-between items-center",
+            "cursor-pointer",
+            "ease-in duration-200",
+            selectedAddress.value === v.Addressid
+              ? "dark:brightness-150 brightness-90"
+              : "dark:hover:brightness-125 hover:brightness-95",
+          )}
+          onClick$={() => (selectedAddress.value = v.Addressid)}
+        >
+          <div class="text-left">
+            {" "}
+            <p>
+              {v.Addressdetails}, {v.Barangay},
+            </p>
+            <p>
+              {v.City}, {v.Province}, {v.Region},
+            </p>
+            <p>{v.Country},</p>
+          </div>
+        </button>
+      ))}
+
+      <Dialog>
+        <DialogTrigger
+          q:slot="trigger"
+          class={cn(
+            "w-full px-5 py-10",
+            "border-2 border-surface",
+            "shadow-sm rounded-md",
+            "flex justify-center items-center",
+            "cursor-pointer",
+            "dark:hover:brightness-125 brightness-95",
+            "bg-transparent hover:bg-surface",
+            "ease-in duration-200",
+          )}
+        >
+          <div class="flex items-center gap-2 bg-transparent">
+            <p class="text-3xl text-input mb-1">+</p>
+            <p class="text-input">Add New Address</p>
+          </div>
+        </DialogTrigger>
+
+        <div>
+          <Heading size="md">New Address</Heading>
+
+          <CreateAddress />
+        </div>
+      </Dialog>
+    </div>
   );
 });
 
