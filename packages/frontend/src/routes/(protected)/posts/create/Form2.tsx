cebaacd56ad65aad18082731c61acebc8d5e257d
marcotheo <marcobutalid989@gmail.com>
2024-11-07 12:42:00 +0800

    form 2

---
+++ packages/frontend/src/routes/(protected)/posts/create/Form2.tsx
@@ -0,0 +1,88 @@
+import { $, component$, useContext, useSignal } from "@builder.io/qwik";
+
+import { useQuery } from "~/hooks/use-query/useQuery";
+import { ListAddressResponse } from "~/common/types";
+import Heading from "~/components/heading/heading";
+import { FormDataCtx, FormStepCtx } from "./index";
+import Button from "~/components/button/button";
+import { cn } from "~/common/utils";
+
+const AddressOptions = component$(() => {
+  const formDataCtx = useContext(FormDataCtx);
+  const activeStep = useContext(FormStepCtx);
+  const selectedAddress = useSignal(formDataCtx.form2 ?? "");
+
+  const handleSubmit = $(() => {
+    try {
+      formDataCtx.form2 = selectedAddress.value;
+      activeStep.value = 3;
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  });
+
+  const { state } = useQuery<ListAddressResponse>(
+    "/address",
+    {},
+    { runOnRender: true },
+  );
+
+  return (
+    <>
+      {state.loading && <div>Loading ...</div>}
+      <div class="space-y-5">
+        {state.result?.data.map((v) => (
+          <button
+            key={v.Addressid}
+            class={cn(
+              "w-full p-5",
+              "shadow-sm bg-surface rounded-md",
+              "flex justify-between items-center",
+              "cursor-pointer",
+              "ease-in duration-200",
+              selectedAddress.value === v.Addressid
+                ? "dark:brightness-150 brightness-90"
+                : "dark:hover:brightness-125 hover:brightness-95",
+            )}
+            onClick$={() => (selectedAddress.value = v.Addressid)}
+          >
+            <div class="text-left">
+              {" "}
+              <p>
+                {v.Addressdetails}, {v.Barangay},
+              </p>
+              <p>
+                {v.City}, {v.Province}, {v.Region},
+              </p>
+              <p>{v.Country},</p>
+            </div>
+          </button>
+        ))}
+      </div>
+    </>
+  );
+});
+
+export default component$(() => {
+  return (
+    <div class="flex h-full w-full justify-center">
+      <div class={cn("px-5 lg:px-24 md:py-12 w-full")}>
+        <Heading class="max-md:hidden">Address Information</Heading>
+
+        <br class="max-md:hidden" />
+
+        <p class="text-gray-500 max-md:hidden">Set address for the job post.</p>
+
+        <br class="max-md:hidden" />
+
+        <AddressOptions />
+
+        <div class="flex justify-end mt-5">
+          <Button type="submit" class="mt-3">
+            Next {"->"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+});
