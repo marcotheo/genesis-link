9c06a92785ad863cce67e82b047f28d271ac1ba6
marcotheo <marcobutalid989@gmail.com>
2024-12-16 02:19:47 +0800

    revert test

--- packages/frontend/src/routes/plugin@csp.ts
+++ packages/frontend/src/routes/plugin@csp.ts
@@ -1,5 +1,5 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
-import { baseCDNUrl } from "~/common/constants";
+import { baseApiUrl, baseCDNUrl } from "~/common/constants";
 import { isDev } from "@builder.io/qwik/build";
 
 export const onRequest: RequestHandler = (event) => {
@@ -10,7 +10,7 @@ export const onRequest: RequestHandler = (event) => {
   const csp = [
     `default-src 'self'`,
     `script-src 'self' 'nonce-${nonce}'`,
-    `connect-src 'self' https://0wozmmq0x9.execute-api.ap-southeast-1.amazonaws.com`,
+    `connect-src 'self' ${baseApiUrl}`,
     `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
     `font-src 'self' https://fonts.gstatic.com`,
     `img-src 'self' 'unsafe-inline' ${baseCDNUrl} data:`,
