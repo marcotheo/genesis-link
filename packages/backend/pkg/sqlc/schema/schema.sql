4c6038734463c6e53bee7ba7abc8fac49b8d8ed7
marcotheo <marcobutalid989@gmail.com>
2024-12-23 12:25:46 +0800

    updated schema.sql file

--- packages/backend/pkg/sqlc/schema/schema.sql
+++ packages/backend/pkg/sqlc/schema/schema.sql
@@ -1,6 +1,8 @@
 CREATE TABLE users (
     userId TEXT NOT NULL PRIMARY KEY,
     email TEXT NOT NULL,
+    mobileNumber TEXT,
+    resumeLink TEXT,
     created_at DEFAULT CURRENT_TIMESTAMP,
     updated_at DEFAULT CURRENT_TIMESTAMP
 );
@@ -70,4 +72,17 @@ CREATE TABLE post_requirements (
 
 CREATE INDEX idx_post_requirements_postId ON post_requirements(postId);
 
+CREATE TABLE applications (
+    applicationId TEXT NOT NULL PRIMARY KEY,
+    resumeLink TEXT,
+    status TEXT CHECK(status IN ('APPLIED', 'REVIEWED', 'SKIPPED', 'FOR_INTERVIEW', 'INTERVIEW', 'REJECTED_AFTER_INTERVIEW', 'JOB_OFFER', 'REJECTED_JOB_OFFER', 'ACCEPTED_JOB_OFFER', 'JOB_UNAVAILABLE')) NOT NULL,
+    postId TEXT NOT NULL,
+    userId TEXT NOT NULL,
+    created_at DEFAULT CURRENT_TIMESTAMP,
+    updated_at DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (postId) REFERENCES posts(postId), 
+    FOREIGN KEY (userId) REFERENCES users(userId)
+)
 
+CREATE INDEX idx_applications_postId ON applications(postId);
+CREATE INDEX idx_applications_userId ON applications(userId);
