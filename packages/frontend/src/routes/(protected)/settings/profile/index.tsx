433366e5ecfdbcb6ce3bf10434068f5795b5b686
marcotheo <marcobutalid989@gmail.com>
2024-12-23 18:39:29 +0800

    update profile settings with user info

--- packages/frontend/src/routes/(protected)/settings/profile/index.tsx
+++ packages/frontend/src/routes/(protected)/settings/profile/index.tsx
@@ -1,5 +1,79 @@
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
 
+import { useAuthHeadersLoader } from "~/routes/layout";
+import { TbEdit } from "@qwikest/icons/tablericons";
+import { cn, qwikFetch } from "~/common/utils";
+import { GetUserAPI } from "~/common/types";
+import dayjs from "dayjs";
+
+export const useAccountDetailsLoader = routeLoader$(
+  async ({ resolveValue }) => {
+    try {
+      const headers = await resolveValue(useAuthHeadersLoader);
+
+      if (!headers) return null;
+
+      const res = await qwikFetch<GetUserAPI>("/users/account/details", {
+        method: "GET",
+        headers,
+      });
+
+      return { ...res.data };
+    } catch (err: any) {
+      console.log("Error:", err);
+
+      return null;
+    }
+  },
+);
+
+const Item = component$<{ label: string; value: string }>(
+  ({ label, value }) => {
+    return (
+      <div class="p-5 w-96 bg-soft relative rounded-md">
+        <div
+          class={cn(
+            "absolute top-3 right-3",
+            "cursor-pointer hover:text-primary duration-300",
+          )}
+        >
+          <TbEdit />
+        </div>
+
+        <p>
+          <span class="font-semibold">{label}:</span> {value}
+        </p>
+      </div>
+    );
+  },
+);
+
 export default component$(() => {
-  return <div>hello</div>;
+  const result = useAccountDetailsLoader();
+
+  return (
+    <div class="mt-5 space-y-5">
+      <Item label="Email" value={result.value?.email ?? ""} />
+      <Item
+        label="Mobile Number"
+        value={!result.value?.mobileNumber ? "N/A" : result.value?.mobileNumber}
+      />
+      <Item label="Resume" value={result.value?.resumelink ?? ""} />
+      <Item
+        label="Registered At"
+        value={dayjs(result.value?.createdAt).format("MMM DD, YYYY")}
+      />
+    </div>
+  );
 });
+
+export const head: DocumentHead = {
+  title: "Profile",
+  meta: [
+    {
+      name: "description",
+      content: "manage profile settings",
+    },
+  ],
+};
