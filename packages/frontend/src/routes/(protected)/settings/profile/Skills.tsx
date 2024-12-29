1553bf0d63014b5854962ad0803d6c5843286970
marcotheo <marcobutalid989@gmail.com>
2024-12-29 16:21:06 +0800

    add skills

--- packages/frontend/src/routes/(protected)/settings/profile/Skills.tsx
+++ packages/frontend/src/routes/(protected)/settings/profile/Skills.tsx
@@ -1,9 +1,221 @@
-import { component$ } from "@builder.io/qwik";
+import {
+  insert,
+  remove,
+  SubmitHandler,
+  useForm,
+  valiForm$,
+} from "@modular-forms/qwik";
+import { $, component$ } from "@builder.io/qwik";
+import * as v from "valibot";
 
+import LoadingOverlay from "~/components/loading-overlay/loading-overlay";
+import Dialog, { DialogTrigger } from "~/components/dialog/dialog";
+import { TbPlus, TbTrash } from "@qwikest/icons/tablericons";
+import { useMutate } from "~/hooks/use-mutate/useMutate";
 import { useQuery } from "~/hooks/use-query/useQuery";
+import { useToast } from "~/hooks/use-toast/useToast";
+import Heading from "~/components/heading/heading";
 import Button from "~/components/button/button";
 import { GetUserSkills } from "~/common/types";
+import Input from "~/components/input/input";
 import { cn } from "~/common/utils";
+
+const schema = v.object({
+  skills: v.array(
+    v.object({
+      skillName: v.pipe(v.string(), v.nonEmpty("Required")),
+      skillLevel: v.string(),
+      skillCategory: v.string(),
+    }),
+  ),
+});
+
+type SchemaType = v.InferInput<typeof schema>;
+
+const SkillForm = component$(() => {
+  const toast = useToast();
+
+  const { mutate } = useMutate("/users/create/skills");
+
+  const [form, { Form, Field, FieldArray }] = useForm<SchemaType>({
+    loader: {
+      value: {
+        skills: [
+          {
+            skillName: "",
+            skillLevel: "",
+            skillCategory: "",
+          },
+        ],
+      },
+    },
+    validate: valiForm$(schema),
+    fieldArrays: ["skills"],
+  });
+
+  const handleSubmit = $<SubmitHandler<SchemaType>>(async (values) => {
+    try {
+      await mutate({
+        skills: [...values.skills],
+      });
+
+      toast.add({
+        title: "Success",
+        message: "Skills Added",
+        type: "success",
+      });
+    } catch (error) {
+      console.error("Error submitting form:", error);
+
+      toast.add({
+        title: "Saving failed",
+        message: typeof error === "string" ? error : "Something Went Wrong",
+        type: "destructive",
+      });
+    }
+  });
+
+  return (
+    <div
+      class={cn(
+        "flex flex-col",
+        "justify-center items-center min-[1100px]:items-end",
+      )}
+    >
+      <Dialog>
+        <DialogTrigger
+          q:slot="trigger"
+          class={cn(
+            "w-full px-5 py-3",
+            "shadow-sm rounded-md",
+            "flex justify-center items-center",
+            "cursor-pointer",
+            "ease-in duration-200",
+          )}
+          variant="ghost"
+        >
+          + Add Skills
+        </DialogTrigger>
+
+        <div>
+          <LoadingOverlay open={form.submitting}>Saving Skills</LoadingOverlay>
+
+          <Form class="flex flex-col gap-5" onSubmit$={handleSubmit}>
+            <FieldArray name="skills">
+              {(fieldArray) => (
+                <div
+                  id={fieldArray.name}
+                  class={cn("rounded-lg", "bg-surface")}
+                >
+                  <Heading>Add Skills</Heading>
+
+                  <div class="py-5 space-y-3 max-h-[30rem] sm:max-h-80 overflow-y-auto">
+                    {fieldArray.items.map((item, idx) => (
+                      <div
+                        key={item}
+                        class={cn(
+                          "flex flex-col min-[550px]:flex-row",
+                          "gap-3 items-end",
+                        )}
+                      >
+                        <Field name={`${fieldArray.name}.${idx}.skillName`}>
+                          {(field, props) => (
+                            <Input
+                              {...props}
+                              label="skill name *"
+                              variant="filled"
+                              errorMsg={field.error}
+                              value={field.value}
+                              class="h-[50px]"
+                              required
+                            />
+                          )}
+                        </Field>
+
+                        <Field name={`${fieldArray.name}.${idx}.skillLevel`}>
+                          {(field, props) => (
+                            <Input
+                              {...props}
+                              label="skill level"
+                              variant="filled"
+                              errorMsg={field.error}
+                              value={field.value}
+                              class="h-[50px]"
+                            />
+                          )}
+                        </Field>
+
+                        <Field name={`${fieldArray.name}.${idx}.skillCategory`}>
+                          {(field, props) => (
+                            <Input
+                              {...props}
+                              label="skill category"
+                              variant="filled"
+                              errorMsg={field.error}
+                              value={field.value}
+                              class="h-[50px]"
+                            />
+                          )}
+                        </Field>
+
+                        <div class="h-[50px]">
+                          <Button
+                            type="button"
+                            class={cn(
+                              "h-full",
+                              "bg-destructive hover:bg-destructive hover:brightness-125",
+                            )}
+                            onClick$={() =>
+                              remove(form, "skills", {
+                                at: idx,
+                              })
+                            }
+                          >
+                            <TbTrash />
+                          </Button>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+
+                  <Button
+                    type="button"
+                    onClick$={() => {
+                      insert(form, "skills", {
+                        value: {
+                          skillName: "",
+                          skillLevel: "",
+                          skillCategory: "",
+                        },
+                      });
+                    }}
+                    class="py-3 px-3"
+                  >
+                    <TbPlus />
+                  </Button>
+                </div>
+              )}
+            </FieldArray>
+
+            <div class="flex justify-end gap-3 mt-5">
+              <DialogTrigger
+                class="px-10 border-input text-input"
+                variant="outline"
+                type="button"
+              >
+                Cancel
+              </DialogTrigger>
+
+              <Button type="submit" class="min-[360px]:px-10">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Dialog>
+    </div>
+  );
+});
 
 const SkillList = component$(() => {
   const { state } = useQuery<GetUserSkills>(
@@ -13,21 +225,18 @@ const SkillList = component$(() => {
   );
 
   return (
-    <div>
+    <div class="h-40 w-full">
       {state.result?.data.skills ? (
-        "hello"
+        <div>
+          {state.result.data.skills.map((v) => (
+            <div class="bg-soft px-5 py-3 rounded-full">{v.skillName}</div>
+          ))}
+        </div>
       ) : (
-        <div
-          class={cn(
-            "w-full flex flex-col",
-            "justify-center items-center min-[1100px]:items-end",
-          )}
-        >
-          <div>
-            <Button class="px-10" variant="ghost">
-              + Add Skills
-            </Button>
-          </div>
+        <div class="h-full w-full flex justify-center">
+          <Heading class="text-gray-500 pt-5 md:pt-10">
+            No skills defined
+          </Heading>
         </div>
       )}
     </div>
@@ -38,15 +247,19 @@ export default component$(() => {
   return (
     <div
       class={cn(
-        "flex flex-col min-[1100px]:flex-row",
+        "flex flex-col",
         "min-[1100px]:items-center justify-between",
         "max-[1100px]:gap-3",
         "py-3",
       )}
     >
-      <div>
-        <h1 class="text-xl font-semibold">Skills</h1>
-        <p class="text-gray-500 text-sm max-md:hidden">review or add skills</p>
+      <div class="flex justify-between w-full">
+        <div class="w-fit whitespace-nowrap">
+          <h1 class="text-xl font-semibold">Skills</h1>
+          <p class="text-gray-500 text-sm">review or add skills</p>
+        </div>
+
+        <SkillForm />
       </div>
 
       <SkillList />
