f5fbfb5d810c73cc86248a3ffb410a9d3d7e7db2
marcotheo <marcobutalid989@gmail.com>
2025-01-05 23:25:06 +0800

    input component polished

--- packages/frontend/src/components/input/input.tsx
+++ packages/frontend/src/components/input/input.tsx
@@ -12,13 +12,19 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const inputVariants = {
   filled: cn(
-    "bg-ghost px-[10px] pt-5 pb-1",
+    "h-12 px-[10px] pt-5 pb-1",
+    "bg-ghost",
     "border-b-[2px] border-r-md border-input",
     "rounded-t-sm",
   ),
-  underline: cn("bg-transparent py-2 border-b-[2px] border-input"),
+  underline: cn(
+    "h-12 px-[10px]",
+    "bg-transparent",
+    "border-b-[2px] border-input",
+  ),
   default: cn(
-    "bg-transparent rounded-md px-[10px] py-2",
+    "h-12 px-[10px]",
+    "bg-transparent rounded-md",
     "border-[1px] hover:border-primary",
     "focus:border-primary focus-visible:border-transparent",
     "focus-visible:ring-2 focus-visible:ring-primary",
@@ -33,27 +39,19 @@ const middleVariant = {
 
 const labelVariants = {
   filled: cn(
-    "top-[12px] ml-[10px] bg-transparent",
-    "translate-x-[-3px] translate-y-[-12px]",
-    "peer-focus:translate-x-[-3px] peer-focus:translate-y-[-12px]",
+    "bg-transparent",
+    "peer-focus:top-3 peer-focus:left-1",
+    "top-3 left-1",
   ),
-  underline: cn(
-    "top-[8px]",
-    "translate-x-[-3px] translate-y-[-17px]",
-    "peer-focus:translate-x-[-3px] peer-focus:translate-y-[-17px]",
-  ),
-  default: cn(
-    "top-[8px] ml-[10px]",
-    "translate-x-[-3px] translate-y-[-17px]",
-    "peer-focus:translate-x-[-3px] peer-focus:translate-y-[-17px]",
-  ),
+  underline: cn("peer-focus:top-0 peer-focus:left-2", "top-0 left-2"),
+  default: cn("peer-focus:top-0 peer-focus:left-2", "top-0 left-2"),
 };
 
 export default component$<InputProps>(
   ({ variant = "default", label, errorMsg, ...props }) => {
     return (
       <div class="w-full">
-        <div class="relative w-full">
+        <div class="relative w-full h-full">
           <input
             {...props}
             value={
@@ -74,6 +72,7 @@ export default component$<InputProps>(
               props.class,
             )}
           />
+
           <div
             class={cn(
               "absolute bottom-0",
@@ -83,14 +82,21 @@ export default component$<InputProps>(
               middleVariant[variant],
             )}
           />
+
           <p
             class={cn(
               "absolute px-1",
-              "text-input text-[0.8em] pointer-events-none",
+              "text-input",
+              "pointer-events-none",
               "duration-100 ease-out",
-              "peer-placeholder-shown:text-[1em]", // default state
-              "peer-placeholder-shown:translate-x-[0] peer-placeholder-shown:translate-y-[0]", // default state
-              "peer-focus:z-[10] peer-focus:text-[0.8em]",
+              "-translate-y-1/2",
+
+              // default state
+              "peer-placeholder-shown:text-[1em]",
+              "peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-3",
+
+              // focus or with value state
+              "text-[0.8em] peer-focus:text-[0.8em] peer-focus:z-[10]",
               labelVariants[variant],
             )}
           >
