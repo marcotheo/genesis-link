2a0d92460f7dd34f55eb8f10d671f6b29b95461e
marcotheo <marcobutalid989@gmail.com>
2025-01-07 12:39:41 +0800

    modified drop down to use qwik ui

--- packages/frontend/src/components/drop-down/drop-down.tsx
+++ packages/frontend/src/components/drop-down/drop-down.tsx
@@ -2,182 +2,62 @@ import {
   component$,
   Slot,
   HTMLAttributes,
-  useSignal,
-  useOnDocument,
   $,
-  Signal,
-  QRL,
   createContextId,
   useContextProvider,
   useContext,
 } from "@builder.io/qwik";
+import { Popover, usePopover } from "@qwik-ui/headless";
 import { Link } from "@builder.io/qwik-city";
 
-import Button from "../button/button";
-import { cn } from "~/common/utils";
+import { cn, generateRandomId } from "~/common/utils";
 
 interface DropDownMenuProps extends HTMLAttributes<HTMLMenuElement> {
-  triggerTitle?: string;
+  panelCss?: string;
+  panelWidth?: string;
 }
 
 export const DropDownContext = createContextId<{
-  isOpen: Signal<boolean | null>;
+  id: string;
 }>("dropdown.context");
 
-export default component$<DropDownMenuProps>(({ triggerTitle, ...props }) => {
-  const isOpen = useSignal<boolean | null>(null);
-  const dropdownRef = useSignal<HTMLDivElement>();
-  const dropDownContentRef = useSignal<HTMLDivElement>();
-
-  const onTrigger = $(() => {
-    if (isOpen.value === null) isOpen.value = true;
-    else isOpen.value = !isOpen.value;
-  });
-
-  const onMenuToggle = $(() => {
-    if (!dropDownContentRef.value || !dropdownRef.value) return;
-
-    const wrapperRect = dropdownRef.value.getBoundingClientRect();
-    const dropdownRect = dropDownContentRef.value.getBoundingClientRect();
-    const overflowY = dropdownRect.bottom > window.innerHeight;
-    const overflowR = dropdownRect.right > window.innerWidth;
-
-    if (overflowR)
-      dropDownContentRef.value.style.right = `${dropdownRect.right - window.innerWidth}px`;
-
-    if (overflowY)
-      dropDownContentRef.value.style.marginTop = `-${wrapperRect.height + dropdownRect.height}px`;
-  });
+export default component$<DropDownMenuProps>(({ panelCss, panelWidth }) => {
+  const popoverId = generateRandomId();
 
   useContextProvider(DropDownContext, {
-    isOpen,
+    id: popoverId,
   });
 
-  useOnDocument(
-    "click",
-    $((event: any) => {
-      if (
-        isOpen.value &&
-        dropdownRef.value &&
-        !dropdownRef.value.contains(event.target as Node)
-      ) {
-        isOpen.value = false;
-      }
-    }),
-  );
-
   return (
-    <menu
-      {...props}
-      class={cn("font-secondary font-medium", "group", props.class)}
-      ref={dropdownRef}
-    >
-      <DropDownMenuTrigger
-        title={triggerTitle}
-        isOpen={isOpen}
-        onToggle={onMenuToggle}
-      />
+    <Popover.Root id={popoverId}>
+      <Popover.Trigger class="group">
+        <Slot name="trigger" />
+      </Popover.Trigger>
 
-      <button
-        class={cn(
-          "bg-surface shadow-lg",
-          "rounded-full h-14 w-14",
-          "flex items-center justify-center",
-          "text-text relative",
-          "hover:brightness-90 dark:hover:brightness-125",
-          "transition-all duration-200 ease-in",
-        )}
-        onClick$={[onTrigger, onMenuToggle]}
-      >
-        <Slot name="trigger-circle-icon" />
-        <div
-          class={cn(
-            "flex items-center justify-center",
-            "bg-surface h-5 w-5  rounded-full",
-            "shadow-lg",
-            "absolute bottom-0 right-0",
-          )}
-        >
-          <ChevronDown isOpen={isOpen.value} />
-        </div>
-      </button>
-
-      <div
-        ref={dropDownContentRef}
+      <Popover.Panel
         class={cn(
           "absolute bg-surface",
           "mt-1 py-1",
           "shadow-md rounded-md",
           "border-[0.5px] border-popup",
-          isOpen.value === null
-            ? "opacity-0 z-[-10]"
-            : isOpen.value
-              ? "animate-fade-in-slide z-50"
-              : "animate-fade-out-slide z-[-10]",
+          "data-[open]:animate-fade-in-slide",
+          panelCss,
+          panelWidth ? panelWidth : "w-fit",
         )}
       >
         <Slot name="label" />
         <div class="flex flex-col overflow-auto max-h-56">
           <Slot />
         </div>
-      </div>
-    </menu>
-  );
-});
-
-const ChevronDown = component$<{ isOpen: boolean | null; class?: string }>(
-  ({ isOpen, ...props }) => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class={cn(
-          "size-4 bg-transparent",
-          "duration-300 ease-out",
-          isOpen ? "rotate-180" : "",
-          props.class,
-        )}
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m19.5 8.25-7.5 7.5-7.5-7.5"
-        />
-      </svg>
-    );
-  },
-);
-
-const DropDownMenuTrigger = component$<{
-  title?: string;
-  isOpen: Signal<boolean | null>;
-  onToggle: QRL<() => void>;
-}>(({ isOpen, title, onToggle }) => {
-  const onTrigger = $(() => {
-    if (isOpen.value === null) isOpen.value = true;
-    else isOpen.value = !isOpen.value;
-  });
-
-  return (
-    <>
-      <Button
-        class={cn("peer flex gap-3 items-center", !title && "hidden")}
-        variant="outline"
-        onClick$={[onTrigger, onToggle]}
-      >
-        {title} <ChevronDown isOpen={isOpen.value} />
-      </Button>
-    </>
+      </Popover.Panel>
+    </Popover.Root>
   );
 });
 
 export const DropDownMenuLabel = component$(() => {
   return (
     <>
-      <div class={cn("p-2 min-w-48")}>
+      <div class={cn("p-2 overflow-auto")}>
         <p class="font-secondary font-semibold text-sm">
           <Slot />
         </p>
@@ -190,34 +70,38 @@ export const DropDownMenuLabel = component$(() => {
 
 export const DropDownMenuItem = component$(() => {
   return (
-    <>
-      <div class="w-full px-1">
-        <div
-          class={cn(
-            "min-w-48 p-2 rounded-md",
-            "cursor-pointer hover:brightness-90 hover:dark:brightness-150",
-            "duration-100 ease-out",
-          )}
-        >
-          <p class="font-semibold text-xs">
-            <Slot />
-          </p>
-        </div>
+    <div class="w-full px-1">
+      <div
+        class={cn(
+          "p-2 rounded-md",
+          "cursor-pointer hover:brightness-90 hover:dark:brightness-150",
+          "duration-100 ease-out",
+        )}
+      >
+        <p class="font-semibold text-xs">
+          <Slot />
+        </p>
       </div>
-    </>
+    </div>
   );
 });
 
 export const DropDownMenuItemLink = component$<{ link: string }>(({ link }) => {
   const dropDownData = useContext(DropDownContext);
+  const { togglePopover } = usePopover(dropDownData.id);
 
   return (
     <>
-      <Link href={link} onClick$={$(() => (dropDownData.isOpen.value = false))}>
+      <Link
+        href={link}
+        onClick$={$(() => {
+          togglePopover();
+        })}
+      >
         <div class="w-full px-1">
           <div
             class={cn(
-              "min-w-48 p-2 rounded-md",
+              "p-2 rounded-md",
               "cursor-pointer hover:brightness-90 hover:dark:brightness-150",
               "duration-100 ease-out",
             )}
