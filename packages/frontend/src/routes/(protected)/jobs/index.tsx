b9fddbb90468cd1293a02b3122b1a7b88996dd1c
marcotheo <marcobutalid989@gmail.com>
2025-01-10 14:19:44 +0800

    search  inputs

--- packages/frontend/src/routes/(protected)/jobs/index.tsx
+++ packages/frontend/src/routes/(protected)/jobs/index.tsx
@@ -1,11 +1,42 @@
+import { TbSearch } from "@qwikest/icons/tablericons";
 import { DocumentHead } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
 
+import Input from "~/components/input/input";
+import { cn } from "~/common/utils";
+
 export default component$(() => {
   return (
-    <div class="overflow-hidden pb-6 w-full flex justify-center items-center">
-      <br />
-      Under Construction
+    <div
+      class={cn(
+        "py-12 w-full",
+        "flex justify-center items-center",
+        "overflow-hidden",
+      )}
+    >
+      <div class="p-1 flex gap-3 w-full">
+        <div class="w-[60%] relative">
+          <div
+            class={cn(
+              "bg-transparent",
+              "absolute z-50",
+              "top-1/2 right-3",
+              "-translate-y-1/2",
+              "h-fit text-2xl",
+            )}
+          >
+            <TbSearch />
+          </div>
+
+          <Input label="Search Job" variant="filled" />
+        </div>
+        <div class="w-[20%]">
+          <Input label="Province" variant="filled" />
+        </div>
+        <div class="w-[20%]">
+          <Input label="City" variant="filled" />
+        </div>
+      </div>
     </div>
   );
 });
