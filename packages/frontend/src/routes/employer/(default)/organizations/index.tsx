b25e1103b89405c8643e9fc5dc7422f175aef3f2
marcotheo <marcobutalid989@gmail.com>
2025-01-16 15:53:39 +0800

    organizations list and select

--- packages/frontend/src/routes/employer/(default)/organizations/index.tsx
+++ packages/frontend/src/routes/employer/(default)/organizations/index.tsx
@@ -1,14 +1,25 @@
 import { Link, type DocumentHead } from "@builder.io/qwik-city";
-import { $, component$, useSignal } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import dayjs from "dayjs";
 
+import { TbFolderOff, TbPlus } from "@qwikest/icons/tablericons";
 import { Pagination } from "~/components/pagination/pagination";
-import { OrgPartialItem } from "~/types/organizations";
 import { useQuery } from "~/hooks/use-query/useQuery";
-import { Table } from "~/components/table/table";
+import Heading from "~/components/heading/heading";
 import Button from "~/components/button/button";
+import { cn } from "~/common/utils";
 
-export default component$(() => {
+const TableSkeleton = component$(() => {
+  return (
+    <>
+      {[...Array(5)].map((_, idx) => (
+        <div class="p-10 bg-soft rounded animate-pulse" />
+      ))}
+    </>
+  );
+});
+
+const Organizations = component$(() => {
   const page = useSignal(1);
 
   const { state } = useQuery(
@@ -29,51 +40,100 @@ export default component$(() => {
     },
   );
 
-  const createadAtRow = $((item: OrgPartialItem) =>
-    dayjs.unix(item.createdAt).format("MMM DD, YYYY"),
-  );
+  if (state.loading)
+    return (
+      <div class="space-y-3">
+        <TableSkeleton />
+      </div>
+    );
 
+  if (state.result?.data.total)
+    return (
+      <>
+        <div class={cn("max-h-96 overflow-y-auto", "space-y-2")}>
+          {state.result.data.organizations.map((v) => (
+            <div
+              class={cn(
+                "bg-surface",
+                "w-full p-8",
+                "rounded-md",
+                "flex justify-between items-center",
+                "duration-200 ease-in",
+                "cursor-pointer",
+                "dark:hover:brightness-110 hover:brightness-90",
+              )}
+            >
+              <Heading size="sm">{v.company}</Heading>
+              <p>{dayjs.unix(v.createdAt).format("MMM DD, YYYY")}</p>
+            </div>
+          ))}
+        </div>
+
+        <div class="flex w-full justify-end">
+          <Pagination
+            totalPages={
+              state.result?.data.total
+                ? Math.ceil(state.result?.data.total / 10)
+                : 0
+            }
+            currentPage={page}
+          />
+        </div>
+      </>
+    );
+
+  return (
+    <div
+      class={cn(
+        "h-96 w-full border-t border-input",
+        "flex items-center justify-center",
+      )}
+    >
+      <div class="flex flex-col items-center gap-3">
+        <div class="text-6xl">
+          <TbFolderOff />
+        </div>
+        <p class="text-gray-500">No company created</p>
+        <Link href="/employer/organizations/create">
+          <Button>Create Company</Button>
+        </Link>
+      </div>
+    </div>
+  );
+});
+
+export default component$(() => {
   return (
     <div class="overflow-hidden pb-6">
       <br />
-      <div class="flex justify-end">
-        <Link href="/posts/create">
-          <Button class="text-3xl" size="sm" variant="outline">
-            +
+      <div class="flex flex-col md:flex-row justify-between md:items-end gap-5">
+        <div class="space-y-1">
+          <Heading size="xl">Companies</Heading>
+          <p class="text-gray-500 text-sm max-w-[22rem] lg:max-w-[30rem]">
+            Please select the company you would like to manage from the
+            dashboard to access its settings and details.
+          </p>
+        </div>
+
+        <Link href="/employer/organizations/create">
+          <Button variant="ghost">
+            <div class="flex gap-3 items-center bg-transparent">
+              <div class="bg-transparent">
+                <TbPlus />{" "}
+              </div>
+              Create Company
+            </div>
           </Button>
         </Link>
       </div>
 
       <br />
 
-      <Table
-        loading={state.loading}
-        data={state.result!.data.organizations}
-        headers={["Company", "CreatedAt"]}
-        rowDef={["company", createadAtRow]}
-        rowKey={"orgId"}
-      />
-
-      <div class="flex w-full justify-end">
-        <Pagination
-          totalPages={
-            state.result?.data.total
-              ? Math.ceil(state.result?.data.total / 10)
-              : 0
-          }
-          currentPage={page}
-        />
-      </div>
+      <Organizations />
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Accounts Posts List",
-  meta: [
-    {
-      name: "description",
-      content: "accounts list of job/volunteering posts created",
-    },
-  ],
+  title: "Ark Point",
 };
