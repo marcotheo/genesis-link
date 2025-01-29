84c4df4e1fabca48da81c8c28cd89edbeaf4b771
marcotheo <marcobutalid989@gmail.com>
2025-01-29 17:35:18 +0800

    job list page ui changes

--- packages/frontend/src/routes/(applicant)/jobs/index.tsx
+++ packages/frontend/src/routes/(applicant)/jobs/index.tsx
@@ -2,6 +2,8 @@ import { TbSearch } from "@qwikest/icons/tablericons";
 import { DocumentHead } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
 
+import ThemedSelect from "~/components/themed-select/themed-select";
+import Heading from "~/components/heading/heading";
 import Input from "~/components/input/input";
 import { cn } from "~/common/utils";
 
@@ -9,11 +11,19 @@ export default component$(() => {
   return (
     <div
       class={cn(
-        "py-12 w-full",
-        "flex justify-center items-center",
+        "py-8 w-full",
+        "flex flex-col gap-5",
+        "justify-center items-start",
         "overflow-hidden",
       )}
     >
+      <div class="space-y-5">
+        <Heading size="xxl">Connecting Ambitions</Heading>
+        <Heading size="xxl">
+          Building <span class="text-primary">Careers</span>
+        </Heading>
+      </div>
+
       <div class="p-1 flex gap-3 w-full">
         <div class="w-[60%] relative">
           <div
@@ -36,17 +46,28 @@ export default component$(() => {
         <div class="w-[20%]">
           <Input label="City" variant="filled" />
         </div>
+        <div class="w-[20%]">
+          <ThemedSelect
+            variant="filled"
+            label="skill level"
+            options={[
+              { label: "Remote", value: "remote" },
+              { label: "Hybrid", value: "hybrid" },
+              { label: "Site", value: "site" },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Ark Point",
+  title: "Ark Point - Jobs",
   meta: [
     {
       name: "description",
-      content: "list of available job post",
+      content: "ark point's list of available jobs",
     },
   ],
 };
