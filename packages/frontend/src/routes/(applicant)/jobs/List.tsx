273f91efbb41c4fd1d7f8435572d86be01de8387
marcotheo <marcobutalid989@gmail.com>
2025-01-31 00:22:35 +0800

    added list of jobs returned from search jobs api

--- packages/frontend/src/routes/(applicant)/jobs/List.tsx
+++ packages/frontend/src/routes/(applicant)/jobs/List.tsx
@@ -9,6 +9,7 @@ import { TbSearchOff } from "@qwikest/icons/tablericons";
 
 import LoadingOverlay from "~/components/loading-overlay/loading-overlay";
 import { useMutate } from "~/hooks/use-mutate/useMutate";
+import Heading from "~/components/heading/heading";
 import { cn } from "~/common/utils";
 import { SearchJobCtx } from ".";
 
@@ -49,20 +50,39 @@ export default component$(() => {
     <>
       <LoadingOverlay open={state.loading}>Searching for jobs</LoadingOverlay>
 
-      <div class={cn("grow w-full relative")}>
-        <div
-          class={cn(
-            "absolute",
-            "top-1/2 left-1/2",
-            "-translate-x-1/2 -translate-y-1/2",
-            "flex flex-col items-center justify-center",
-          )}
-        >
-          <TbSearchOff font-size="100px" />
-          <p class={cn("text-gray-500 text-xl text-center")}>
-            Apply filters to search for jobs
-          </p>
-        </div>
+      <div class={cn("grow w-full relative py-5")}>
+        {state.result?.data.posts && state.result.data.posts.length > 0 ? (
+          <div class="space-y-5">
+            {state.result.data.posts.map((v) => (
+              <div key={v.postId}>
+                <div
+                  class={cn(
+                    "w-full px-5 py-8",
+                    "border-t border-soft",
+                    "animate-fade-in-slide",
+                  )}
+                >
+                  <Heading>{v.title}</Heading>
+                  <p>{v.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div
+            class={cn(
+              "absolute",
+              "top-1/2 left-1/2",
+              "-translate-x-1/2 -translate-y-1/2",
+              "flex flex-col items-center justify-center",
+            )}
+          >
+            <TbSearchOff font-size="100px" />
+            <p class={cn("text-gray-500 text-xl text-center")}>
+              Apply filters to search for jobs
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
