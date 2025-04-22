b8d4fb75a134bd3630e554ce4c5a89a0c13b8326
marcotheo <marcobutalid989@gmail.com>
2025-04-22 17:10:07 +0800

    added loading state

--- packages/frontend/src/routes/(applicant)/jobs/[postId]/index.tsx
+++ packages/frontend/src/routes/(applicant)/jobs/[postId]/index.tsx
@@ -100,8 +100,6 @@ export default component$(() => {
     },
   );
 
-  if (state.loading) return <div>loading ...</div>;
-
   return (
     <div class="h-full">
       <div class={cn("h-full", "flex flex-col gap-10", "pb-10")}>
@@ -114,16 +112,24 @@ export default component$(() => {
           )}
         >
           <div>
-            <p class="text-sm text-input">
-              {"Posted " + timeAgo(state.result?.data.postedAt ?? 0)}
-            </p>
+            {state.loading ? (
+              <div class="px-3 py-2 animate-pulse bg-soft w-40 h-2 rounded-md mb-1" />
+            ) : (
+              <p class="text-sm text-input">
+                {"Posted " + timeAgo(state.result?.data.postedAt ?? 0)}
+              </p>
+            )}
 
             <div
               class={cn("flex flex-col lg:flex-row", "lg:items-center gap-5")}
             >
-              <Heading class="text-3xl md:text-5xl">
-                {state.result?.data.title}
-              </Heading>
+              {state.loading ? (
+                <div class="px-3 py-4 animate-pulse bg-soft w-56 h-14 rounded-md" />
+              ) : (
+                <Heading class="text-3xl md:text-5xl">
+                  {state.result?.data.title}
+                </Heading>
+              )}
 
               <div
                 class={cn(
@@ -131,31 +137,37 @@ export default component$(() => {
                   "lg:pl-5 lg:border-l lg:border-l-text",
                 )}
               >
-                <div
-                  class={cn(
-                    "flex flex-col min-[500px]:flex-row",
-                    "min-[500px]:items-center min-[500px]:gap-3",
-                    "text-lg font-medium",
-                  )}
-                >
-                  <p>{state.result?.data.company}</p>
-                  <div class="w-2 h-2 rounded-full bg-text max-[500px]:hidden" />
-                  <p>
-                    {state.result?.data.jobType} -{" "}
-                    {state.result?.data.workSetup}
-                  </p>
-                </div>
-                <p class="text-sm text-input">
-                  {state.result?.data.salaryCurrency}{" "}
-                  {formatNumberWithCommas(
-                    state.result?.data.salaryAmountMin ?? 0,
-                  )}{" "}
-                  -{" "}
-                  {formatNumberWithCommas(
-                    state.result?.data.salaryAmountMax ?? 0,
-                  )}{" "}
-                  | {state.result?.data.country}, {state.result?.data.city}
-                </p>
+                {state.loading ? (
+                  <div class="px-3 py-4 animate-pulse bg-soft w-72 h-14 rounded-md" />
+                ) : (
+                  <>
+                    <div
+                      class={cn(
+                        "flex flex-col min-[500px]:flex-row",
+                        "min-[500px]:items-center min-[500px]:gap-3",
+                        "text-lg font-medium",
+                      )}
+                    >
+                      <p>{state.result?.data.company}</p>
+                      <div class="w-2 h-2 rounded-full bg-text max-[500px]:hidden" />
+                      <p>
+                        {state.result?.data.jobType} -{" "}
+                        {state.result?.data.workSetup}
+                      </p>
+                    </div>
+                    <p class="text-sm text-input">
+                      {state.result?.data.salaryCurrency}{" "}
+                      {formatNumberWithCommas(
+                        state.result?.data.salaryAmountMin ?? 0,
+                      )}{" "}
+                      -{" "}
+                      {formatNumberWithCommas(
+                        state.result?.data.salaryAmountMax ?? 0,
+                      )}{" "}
+                      | {state.result?.data.country}, {state.result?.data.city}
+                    </p>
+                  </>
+                )}
               </div>
             </div>
           </div>
@@ -172,49 +184,97 @@ export default component$(() => {
         <div class={cn("flex flex-col gap-10", "overflow-y-auto grow pr-1")}>
           {/* post tags */}
           <div class="flex flex-wrap gap-3 items-center">
-            {state.result?.data.tags.map((v) => (
-              <div key={v} class="rounded-full px-3 py-1 bg-soft text-sm">
-                {v}
-              </div>
-            ))}
+            {state.loading ? (
+              <>
+                <div class="px-3 py-4 animate-pulse bg-soft w-24 h-5 rounded-full" />
+                <div class="px-3 py-4 animate-pulse bg-soft w-24 h-5 rounded-full" />
+                <div class="px-3 py-4 animate-pulse bg-soft w-24 h-5 rounded-full" />
+              </>
+            ) : (
+              state.result?.data.tags.map((v) => (
+                <div key={v} class="rounded-full px-3 py-1 bg-soft text-sm">
+                  {v}
+                </div>
+              ))
+            )}
           </div>
 
           {/* post overview */}
           <div class="space-y-1">
             <Heading class="text-2xl md:text-4xl">Position Overview</Heading>
-            <p class="leading-8">{state.result?.data.description}</p>
+            {state.loading ? (
+              <div class="space-y-2">
+                <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              </div>
+            ) : (
+              <p class="leading-8">{state.result?.data.description}</p>
+            )}
           </div>
 
           {/* post Responsibilities and Qualifications */}
           <div>
             <Heading class="text-xl md:text-3xl">Responsibilities:</Heading>
-            <ul>
-              {state.result?.data.requirements
-                .filter((v) => v.requirementType === "responsibility")
-                .map((v, idx) => (
-                  <li key={idx} class="text-lg">
-                    {v.requirement}
-                  </li>
-                ))}
-            </ul>
+            {state.loading ? (
+              <div class="space-y-2">
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+              </div>
+            ) : (
+              <ul>
+                {state.result?.data.requirements
+                  .filter((v) => v.requirementType === "responsibility")
+                  .map((v, idx) => (
+                    <li key={idx} class="text-lg">
+                      {v.requirement}
+                    </li>
+                  ))}
+              </ul>
+            )}
           </div>
 
           <div>
             <Heading class="text-xl md:text-3xl">Qualifications:</Heading>
-            <ul>
-              {state.result?.data.requirements
-                .filter((v) => v.requirementType === "qualification")
-                .map((v, idx) => (
-                  <li key={idx} class="text-lg">
-                    {v.requirement}
-                  </li>
-                ))}
-            </ul>
+            {state.loading ? (
+              <div class="space-y-2">
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+                <div class="px-3 py-2 animate-pulse bg-soft w-72 h-2 rounded-md" />
+              </div>
+            ) : (
+              <ul>
+                {state.result?.data.requirements
+                  .filter((v) => v.requirementType === "qualification")
+                  .map((v, idx) => (
+                    <li key={idx} class="text-lg">
+                      {v.requirement}
+                    </li>
+                  ))}
+              </ul>
+            )}
+            <ul></ul>
           </div>
 
           {/* additional info html content */}
-          {state.result?.data.additionalInfoLink && (
-            <AdditionalHtml htmlLink={state.result.data.additionalInfoLink} />
+          {state.loading ? (
+            <div class="space-y-2">
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+              <div class="px-3 py-2 animate-pulse bg-soft w-full h-2 rounded-md" />
+            </div>
+          ) : (
+            state.result?.data.additionalInfoLink && (
+              <AdditionalHtml htmlLink={state.result.data.additionalInfoLink} />
+            )
           )}
         </div>
       </div>
