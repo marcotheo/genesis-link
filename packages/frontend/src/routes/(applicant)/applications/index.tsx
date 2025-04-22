ea057e4472d011dafb16bab72ac0ac7a96450ff9
marcotheo <marcobutalid989@gmail.com>
2025-04-22 18:12:44 +0800

    initialize applications list view for applicant

---
+++ packages/frontend/src/routes/(applicant)/applications/index.tsx
@@ -0,0 +1,66 @@
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import { $, component$, useSignal } from "@builder.io/qwik";
+import dayjs from "dayjs";
+
+import { Pagination } from "~/components/pagination/pagination";
+import { Table } from "~/components/table/table";
+import Button from "~/components/button/button";
+
+import { GetApplicationsByUserIdApi } from "~/types/application";
+import { useQuery } from "~/hooks/use-query/useQuery";
+
+export default component$(() => {
+  const page = useSignal(1);
+
+  const { state } = useQuery(
+    "GET /applications/applicant",
+    {
+      queryStrings: {
+        page,
+      },
+    },
+    {
+      defaultValues: {
+        status: "",
+        message: "",
+        data: {
+          total: 0,
+          applications: [],
+        },
+      },
+      runOnRender: true,
+    },
+  );
+
+  const DeadlineRow = $(
+    (item: GetApplicationsByUserIdApi["response"]["data"]["applications"][0]) =>
+      dayjs.unix(item.createdAt).format("MMM DD, YYYY"),
+  );
+
+  return (
+    <div class="overflow-hidden pb-6">
+      <Table
+        loading={state.loading}
+        data={state?.result?.data.applications ?? []}
+        headers={["Company", "Title", "Status", "CreatedAt"]}
+        rowKey={"postId"}
+        rowDef={["company", "title", "status", DeadlineRow]}
+      />
+
+      <div class="flex w-full justify-end">
+        <Pagination
+          totalPages={
+            state.result?.data.total
+              ? Math.ceil(state.result?.data.total / 5)
+              : 0
+          }
+          currentPage={page}
+        />
+      </div>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "ArkPoint",
+};
