e24c11d67416d7b821d0c33df10259885e9ece82
marcotheo <marcobutalid989@gmail.com>
2025-04-23 18:08:25 +0800

    initialize applications page for a post

--- packages/frontend/src/routes/employer/organizations/dashboard/[orgId]/posts/[postId]/index.tsx
+++ packages/frontend/src/routes/employer/organizations/dashboard/[orgId]/posts/[postId]/index.tsx
@@ -1,5 +1,93 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import dayjs from "dayjs";
+
+import { GetApplicationsByPostIdApi } from "~/types/application";
+import { Pagination } from "~/components/pagination/pagination";
+import { useQuery } from "~/hooks/use-query/useQuery";
+import Heading from "~/components/heading/heading";
+import { Table } from "~/components/table/table";
+
+export const usePostId = routeLoader$(({ params }) => {
+  const { postId } = params; // Extract the route parameter
+
+  return {
+    postId,
+  };
+});
 
 export default component$(() => {
-  return <div>hello</div>;
+  const pathParams = usePostId();
+  const page = useSignal(1);
+
+  const { state } = useQuery(
+    "GET /posts/{postId}/applications",
+    {
+      queryStrings: {
+        page,
+      },
+      pathParams: {
+        postId: pathParams.value.postId,
+      },
+    },
+    {
+      defaultValues: {
+        status: "",
+        message: "",
+        data: {
+          total: 0,
+          applications: [],
+        },
+      },
+      runOnRender: true,
+    },
+  );
+
+  const CreatedAtRow = $(
+    (item: GetApplicationsByPostIdApi["response"]["data"]["applications"][0]) =>
+      dayjs.unix(item.createdAt).format("MMM DD, YYYY"),
+  );
+
+  return (
+    <div>
+      <Heading>Applications</Heading>
+
+      <br />
+
+      <div>
+        <Table
+          loading={state.loading}
+          data={state?.result?.data.applications ?? []}
+          headers={[
+            "Name",
+            "Resume",
+            "Email",
+            "MobileNumber",
+            "Status",
+            "CreatedAt",
+          ]}
+          rowKey={"applicationId"}
+          rowDef={[
+            "name",
+            "resumeLink",
+            "email",
+            "mobileNumber",
+            "status",
+            CreatedAtRow,
+          ]}
+        />
+
+        <div class="flex w-full justify-end">
+          <Pagination
+            totalPages={
+              state.result?.data.total
+                ? Math.ceil(state.result?.data.total / 5)
+                : 0
+            }
+            currentPage={page}
+          />
+        </div>
+      </div>
+    </div>
+  );
 });
