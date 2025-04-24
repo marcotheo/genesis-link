02ad157c05c0f82ee2beec4fc993af165b6a3285
marcotheo <marcobutalid989@gmail.com>
2025-04-24 16:45:36 +0800

    modified applications table to have menu rows

--- packages/frontend/src/routes/employer/organizations/dashboard/[orgId]/posts/[postId]/Applications.tsx
+++ packages/frontend/src/routes/employer/organizations/dashboard/[orgId]/posts/[postId]/Applications.tsx
@@ -1,5 +1,5 @@
+import { $, component$, useSignal, useTask$ } from "@builder.io/qwik";
 import { TbDotsVertical } from "@qwikest/icons/tablericons";
-import { $, component$, useSignal } from "@builder.io/qwik";
 import { Modal } from "@qwik-ui/headless";
 import dayjs from "dayjs";
 
@@ -17,6 +17,98 @@ import { GetApplicationsByPostIdApi } from "~/types/application";
 import { cn, createDashboardPath } from "~/common/utils";
 import { useQuery } from "~/hooks/use-query/useQuery";
 import { usePathParams } from "../../../layout";
+
+const RowMenu = component$<{
+  item: GetApplicationsByPostIdApi["response"]["data"]["applications"][0];
+}>(({ item }) => {
+  const pathParams = usePathParams();
+  const open = useSignal(false);
+  const resumeURL = useSignal("");
+
+  const fetchResume = $(async () => {
+    if (item.resumeLink) {
+      const imageResponse = await fetch(item.resumeLink.URL, {
+        method: item.resumeLink.Method,
+        headers: item.resumeLink.SignedHeader,
+      });
+
+      if (!imageResponse.ok) {
+        return "Error";
+      }
+
+      const imageBlob = await imageResponse.blob();
+      resumeURL.value = URL.createObjectURL(imageBlob);
+    }
+  });
+
+  useTask$(({ track }) => {
+    track(item);
+    fetchResume();
+  });
+
+  return (
+    <div>
+      <Menu panelWidth="w-56">
+        <div
+          q:slot="trigger"
+          class={cn(
+            "bg-transparent hover:bg-soft",
+            "rounded-md h-10 w-10",
+            "flex items-center justify-center",
+            "text-text text-xl relative",
+            "hover:brightness-90 dark:hover:brightness-125",
+            "transition-all duration-200 ease-in",
+          )}
+        >
+          <TbDotsVertical />
+        </div>
+
+        <DropDownMenuItemLink
+          link={createDashboardPath(pathParams.value.orgId, "")}
+        >
+          View Profile
+        </DropDownMenuItemLink>
+        <DropDownSeparator />
+        <DropDownMenuItem>
+          {resumeURL ? (
+            <a
+              href={resumeURL.value}
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+            >
+              Download CV
+            </a>
+          ) : (
+            "No CV"
+          )}
+        </DropDownMenuItem>
+        <DropDownSeparator />
+        <DropDownMenuItem>
+          <Modal.Root bind:show={open}>
+            <Modal.Trigger class={cn("text-text")}>View Proposal</Modal.Trigger>
+
+            <TModal.Content
+              size="lg"
+              modalTitle="Applicant Proposal"
+              modalDescription="This proposal highlights the applicant's interest, experience, and fit for the role."
+            >
+              <div class="flex flex-col gap-5 max-w-[47rem]">
+                <div class="space-y-1">
+                  <p>proposal Here</p>
+                </div>
+
+                <div class="flex justify-end gap-3 mt-5">
+                  <TModal.Close class="min-[360px]:px-10">Close</TModal.Close>
+                </div>
+              </div>
+            </TModal.Content>
+          </Modal.Root>
+        </DropDownMenuItem>
+      </Menu>
+    </div>
+  );
+});
 
 export default component$(() => {
   const pathParams = usePathParams();
@@ -45,131 +137,6 @@ export default component$(() => {
     },
   );
 
-  const MenuRow = $(
-    async (
-      item: GetApplicationsByPostIdApi["response"]["data"]["applications"][0],
-    ) => {
-      const open = useSignal(false);
-
-      let imageObjectURL = "";
-
-      if (item.resumeLink) {
-        const imageResponse = await fetch(item.resumeLink.URL, {
-          method: item.resumeLink.Method,
-          headers: item.resumeLink.SignedHeader,
-        });
-
-        if (!imageResponse.ok) {
-          return "Error";
-        }
-
-        const imageBlob = await imageResponse.blob();
-        imageObjectURL = URL.createObjectURL(imageBlob);
-      }
-
-      return (
-        <div>
-          <Menu panelWidth="w-56">
-            <div
-              q:slot="trigger"
-              class={cn(
-                "bg-surface shadow-lg",
-                "rounded-full h-14 w-14",
-                "flex items-center justify-center",
-                "text-text relative",
-                "hover:brightness-90 dark:hover:brightness-125",
-                "transition-all duration-200 ease-in",
-              )}
-            >
-              <TbDotsVertical />
-            </div>
-
-            <DropDownMenuItemLink
-              link={createDashboardPath(pathParams.value.orgId, "")}
-            >
-              View Profile
-            </DropDownMenuItemLink>
-            <DropDownSeparator />
-            <DropDownMenuItem>
-              {imageObjectURL ? (
-                <a
-                  href={imageObjectURL}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  download
-                >
-                  Download CV
-                </a>
-              ) : (
-                "No CV"
-              )}
-            </DropDownMenuItem>
-            <DropDownSeparator />
-            <DropDownMenuItem>
-              <Modal.Root bind:show={open}>
-                <Modal.Trigger class={cn("text-text")}>
-                  View Proposal
-                </Modal.Trigger>
-
-                <TModal.Content
-                  size="lg"
-                  modalTitle="Applicant Proposal"
-                  modalDescription="This proposal highlights the applicant's interest, experience, and fit for the role."
-                >
-                  <div class="flex flex-col gap-5 max-w-[47rem]">
-                    <div class="space-y-1">
-                      <p>proposal Here</p>
-                    </div>
-
-                    <div class="flex justify-end gap-3 mt-5">
-                      <TModal.Close class="min-[360px]:px-10">
-                        Close
-                      </TModal.Close>
-                    </div>
-                  </div>
-                </TModal.Content>
-              </Modal.Root>
-            </DropDownMenuItem>
-          </Menu>
-        </div>
-      );
-    },
-  );
-
-  const Resume = $(
-    async (
-      item: GetApplicationsByPostIdApi["response"]["data"]["applications"][0],
-    ) => {
-      if (item.resumeLink) {
-        const imageResponse = await fetch(item.resumeLink.URL, {
-          method: item.resumeLink.Method,
-          headers: item.resumeLink.SignedHeader,
-        });
-
-        if (!imageResponse.ok) {
-          return "Error";
-        }
-
-        const imageBlob = await imageResponse.blob();
-        const imageObjectURL = URL.createObjectURL(imageBlob);
-
-        return (
-          <a
-            href={imageObjectURL}
-            target="_blank"
-            rel="noopener noreferrer"
-            download
-            class="text-blue-600 underline underline-offset-4"
-          >
-            Download CV
-          </a>
-        );
-      }
-
-      return "N/A";
-    },
-  );
-
   const CreatedAtRow = $(
     (item: GetApplicationsByPostIdApi["response"]["data"]["applications"][0]) =>
       dayjs.unix(item.createdAt).format("MMM DD, YYYY"),
@@ -183,25 +150,12 @@ export default component$(() => {
         <Table
           loading={state.loading}
           data={state?.result?.data.applications ?? []}
-          headers={[
-            "Name",
-            "Resume",
-            "Email",
-            "MobileNumber",
-            "Status",
-            "CreatedAt",
-            "",
-          ]}
+          headers={["Name", "Email", "MobileNumber", "Status", "CreatedAt"]}
           rowKey={"applicationId"}
-          rowDef={[
-            "name",
-            Resume,
-            "email",
-            "mobileNumber",
-            "status",
-            CreatedAtRow,
-            MenuRow,
-          ]}
+          rowDef={["name", "email", "mobileNumber", "status", CreatedAtRow]}
+          renderMenu$={$((row: any) => (
+            <RowMenu item={row} />
+          ))}
         />
 
         <div class="flex w-full justify-end">
