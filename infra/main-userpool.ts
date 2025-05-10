6efe74f77b420bc27a9580e07824d56c7dce2b07
marcotheo <marcobutalid989@gmail.com>
2025-05-10 18:18:52 +0800

    userpool resource updates

--- infra/main-userpool.ts
+++ infra/main-userpool.ts
@@ -1,4 +1,3 @@
-import path = require("path");
 import { getSiteUrl } from "./utils";
 
 export const main_user_pool = () => {
@@ -72,19 +71,54 @@ export const main_user_pool = () => {
     },
   });
 
-  const client = new aws.cognito.UserPoolClient("UserPoolClient", {
+  // enable cognito domain for oauth logins (3rd party IDP's)
+  new aws.cognito.UserPoolDomain("UserPoolDomain", {
+    domain: process.env.APP_NAME, // e.g., "myapp-auth"
     userPoolId: pool.id,
-    explicitAuthFlows: ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"],
-    tokenValidityUnits: {
-      accessToken: "minutes",
-      idToken: "minutes",
-      refreshToken: "hours",
-    },
-    accessTokenValidity: 10,
-    idTokenValidity: 10,
-    refreshTokenValidity: 3,
-    generateSecret: true,
   });
+
+  const googleIdp = new aws.cognito.IdentityProvider("GoogleProvider", {
+    userPoolId: pool.id,
+    providerName: "Google",
+    providerType: "Google",
+    providerDetails: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      authorize_scopes: "openid email profile",
+    },
+    attributeMapping: {
+      email: "email",
+      given_name: "given_name",
+      family_name: "family_name",
+    },
+  });
+
+  const client = new aws.cognito.UserPoolClient(
+    "UserPoolClient",
+    {
+      userPoolId: pool.id,
+      explicitAuthFlows: [
+        "ALLOW_USER_PASSWORD_AUTH",
+        "ALLOW_REFRESH_TOKEN_AUTH",
+      ],
+      tokenValidityUnits: {
+        accessToken: "minutes",
+        idToken: "minutes",
+        refreshToken: "hours",
+      },
+      accessTokenValidity: 10,
+      idTokenValidity: 10,
+      refreshTokenValidity: 3,
+      generateSecret: true,
+
+      // google or 3rd party logins
+      allowedOauthFlows: ["code"],
+      allowedOauthScopes: ["email", "openid", "profile"],
+      callbackUrls: [process.env.OAUTH_IDP_REDIRECT_URI],
+      supportedIdentityProviders: ["COGNITO", "Google"],
+    },
+    { dependsOn: [googleIdp] }
+  );
 
   // Grant permission for Cognito to invoke the Lambda function
   new aws.lambda.Permission("CognitoLambdaInvokePermission", {
